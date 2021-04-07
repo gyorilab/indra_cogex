73a009a282d99e2edd4400ef4276911722b8c957
Ben Gyori <ben.gyori@gmail.com>
2021-04-07 00:34:33 -0400

    Add basic object model for representation

---
+++ src/indra_cogex/representation.py
@@ -0,0 +1,36 @@
+__all__ = ['Node', 'Relation']
+
+
+class Node:
+    def __init__(self, identifier, labels, data=None):
+        self.identifier = identifier
+        self.labels = labels
+        self.data = data if data else {}
+
+    def __str__(self):
+        data_str = (', '.join(['%s:\'%s\'' % (k, v)
+                    for k, v in [('id', self.identifier)] +
+                               list(self.data.items())]))
+        labels_str = ':'.join(self.labels)
+        return f'(:{labels_str} {data_str})'
+
+    def __repr__(self):
+        return str(self)
+
+
+class Relation:
+    def __init__(self, source_id, target_id, labels, data):
+        self.source_id = source_id
+        self.target_id = target_id
+        self.labels = labels
+        self.data = data
+
+    def __str__(self):
+        data_str = (', '.join(['%s:\'%s\'' % (k, v)
+                               for k, v in self.data.items()]))
+        labels_str = ':'.join(self.labels)
+        return f'({self.source_id})-[:{labels_str} {data_str}]->' \
+               f'({self.target_id})'
+
+    def __repr__(self):
+        return str(self)
