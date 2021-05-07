4d02cb085679134dbae8a8f7d44fc6e924160674
Ben Gyori <ben.gyori@gmail.com>
2021-05-06 22:45:42 -0400

    Fix sorting key for classes

--- src/indra_cogex/sources/cli.py
+++ src/indra_cogex/sources/cli.py
@@ -3,6 +3,7 @@
 """Run the sources CLI."""
 
 import click
+import operator
 from more_click import verbose_option
 
 from . import processor_resolver
@@ -11,7 +12,8 @@ from . import processor_resolver
 @click.command()
 @verbose_option
 def main():
-    for processor_cls in sorted(processor_resolver.classes):
+    for processor_cls in sorted(processor_resolver.classes,
+                                key=operator.attrgetter('__name__')):
         click.secho(f'Processing {processor_cls.name}', fg='green', bold=True)
         processor = processor_cls()
         _node_path, _edge_path = processor.dump()
