e81fe70dbcd93e9e990cf2e293c5e431eb329de8
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-05-11 12:54:25 -0400

    Get more default variables

--- import.sh
+++ import.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 NEO4J_VERSION=$(neo4j version | cut -f 2 -d ' ')
-NEO4J_PATH=/usr/local/Cellar/neo4j/$NEO4J_VERSION
-NEO4J_DATA=/usr/local/var/neo4j/data
+NEO4J_CONFIG=$(neo4j console | grep "^\s*config" | sed 's/^[ \t]*data:[ \t]*//g')
+NEO4J_DATA=$(neo4j console | grep "^\s*data" | sed 's/^[ \t]*data:[ \t]*//g')
 
 # Output commands as you go
 set -x
@@ -14,7 +14,7 @@ rm -rf $NEO4J_DATA/databases/indra
 rm -rf $NEO4J_DATA/transactions/indra
 
 # Just show what it is. This should match the --database option used below
-cat $NEO4J_PATH/libexec/conf/neo4j.conf | grep "dbms\.default_database"
+cat $NEO4J_CONFIG/neo4j.conf | grep "dbms\.default_database"
 
 python -m indra_cogex.sources --load
 
