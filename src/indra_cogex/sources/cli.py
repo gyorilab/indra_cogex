16cc9c2845ce1ed7f8eb784098c68ff2280dc473
Ben Gyori <ben.gyori@gmail.com>
2021-05-11 12:36:27 -0400

    Revert parameterizing relationships vs edges

--- src/indra_cogex/sources/cli.py
+++ src/indra_cogex/sources/cli.py
@@ -28,16 +28,10 @@ from . import processor_resolver
     is_flag=True,
     help="If true, rebuild all resources",
 )
-@click.option(
-    "--neo4j_old",
-    is_flag=True,
-    help="If True, we assume an older version of neo4j which uses relationships instead of edges",
-)
 @verbose_option
-def main(load: bool, load_only: bool, force: bool, neo4j_old: bool):
+def main(load: bool, load_only: bool, force: bool):
     """Generate and import Neo4j nodes and edges tables."""
     paths = []
-    edges_rel = 'relationships' if neo4j_old else 'edges'
     for processor_cls in processor_resolver:
         if getattr(processor_cls, "importable") is False:
             continue
@@ -64,7 +58,7 @@ def main(load: bool, load_only: bool, force: bool, neo4j_old: bool):
         """
         ).rstrip()
         for node_path, edge_path in paths:
-            command += f"\\\n  --nodes {node_path} \\\n  --{edges_rel} {edge_path}"
+            command += f"\\\n  --nodes {node_path} \\\n  --relationships {edge_path}"
 
         click.secho("Running shell command:")
         click.secho(command, fg="blue")
