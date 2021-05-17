99fb458582fcdb91b4c08d4c6022ff1ac6a0506d
Ben Gyori <ben.gyori@gmail.com>
2021-05-16 23:26:03 -0400

    Fix tests for new API

--- tests/test_neo4j_client.py
+++ tests/test_neo4j_client.py
@@ -1,3 +1,5 @@
+import pytest
+
 from indra.config import get_config
 from indra.statements import Agent
 
@@ -9,32 +11,38 @@ nc = Neo4jClient(
 )
 
 
+@pytest.mark.nonpublic
 def test_get_targets():
-    targets = nc.get_targets("HGNC:6871", "isa")
+    targets = nc.get_targets(("HGNC", "6871"), relation="isa")
     assert len(targets) == 1
-    assert targets[0]["name"] == "ERK"
+    assert targets[0].data["name"] == "ERK"
 
     agent = nc.node_to_agent(targets[0])
     assert isinstance(agent, Agent)
 
 
+@pytest.mark.nonpublic
 def test_get_target_agents():
-    targets = nc.get_target_agents("HGNC:6871", "isa")
+    targets = nc.get_target_agents(("HGNC", "6871"), "isa")
     assert len(targets) == 1
     assert isinstance(targets[0], Agent)
 
 
+@pytest.mark.nonpublic
 def test_get_common_targets():
-    targets1 = nc.get_targets("HGNC:7597", "expressed_in")
-    targets2 = nc.get_targets("HGNC:7599", "expressed_in")
-    targets = nc.get_common_targets(["HGNC:7597", "HGNC:7599"], "expressed_in")
+    targets1 = nc.get_targets(("HGNC", "7597"), "expressed_in")
+    targets2 = nc.get_targets(("HGNC", "7599"), "expressed_in")
+    targets = nc.get_common_targets(
+        [("HGNC", "7597"), ("HGNC", "7599")], "expressed_in"
+    )
     assert len(targets) > 200
-    assert {t["id"] for t in targets1} & {t["id"] for t in targets2} == {
-        t["id"] for t in targets
+    assert {t.db_id for t in targets1} & {t.db_id for t in targets2} == {
+        t.db_id for t in targets
     }
 
 
+@pytest.mark.nonpublic
 def test_get_sources():
-    sources = nc.get_sources("FPLX:ERK", "isa")
+    sources = nc.get_sources(("FPLX", "ERK"), "isa")
     assert len(sources) == 2
-    assert {s["id"] for s in sources} == {"MAPK1", "MAPK3"}, sources
+    assert {s.data["name"] for s in sources} == {"MAPK1", "MAPK3"}, sources
