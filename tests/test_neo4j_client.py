aee7cfe379a57da32b677220a3186acbf20a35b5
Ben Gyori <ben.gyori@gmail.com>
2021-05-17 16:15:13 -0400

    Wrap client creation in function

--- tests/test_neo4j_client.py
+++ tests/test_neo4j_client.py
@@ -5,14 +5,18 @@ from indra.statements import Agent
 
 from indra_cogex.neo4j_client import Neo4jClient
 
-nc = Neo4jClient(
-    get_config("INDRA_NEO4J_URL"),
-    auth=(get_config("INDRA_NEO4J_USER"), get_config("INDRA_NEO4J_PASSWORD")),
-)
+
+def _get_client():
+    nc = Neo4jClient(
+        get_config("INDRA_NEO4J_URL"),
+        auth=(get_config("INDRA_NEO4J_USER"), get_config("INDRA_NEO4J_PASSWORD")),
+    )
+    return nc
 
 
 @pytest.mark.nonpublic
 def test_get_targets():
+    nc = _get_client()
     targets = nc.get_targets(("HGNC", "6871"), relation="isa")
     assert len(targets) == 1
     assert targets[0].data["name"] == "ERK"
@@ -23,6 +27,7 @@ def test_get_targets():
 
 @pytest.mark.nonpublic
 def test_get_target_agents():
+    nc = _get_client()
     targets = nc.get_target_agents(("HGNC", "6871"), "isa")
     assert len(targets) == 1
     assert isinstance(targets[0], Agent)
@@ -30,6 +35,7 @@ def test_get_target_agents():
 
 @pytest.mark.nonpublic
 def test_get_common_targets():
+    nc = _get_client()
     targets1 = nc.get_targets(("HGNC", "7597"), "expressed_in")
     targets2 = nc.get_targets(("HGNC", "7599"), "expressed_in")
     targets = nc.get_common_targets(
@@ -43,6 +49,7 @@ def test_get_common_targets():
 
 @pytest.mark.nonpublic
 def test_get_sources():
+    nc = _get_client()
     sources = nc.get_sources(("FPLX", "ERK"), "isa")
     assert len(sources) == 2
     assert {s.data["name"] for s in sources} == {"MAPK1", "MAPK3"}, sources
