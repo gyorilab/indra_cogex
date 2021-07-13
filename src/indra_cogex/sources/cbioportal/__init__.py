6a0b86c205d9f3b92fe8c946445a62da9797f160
Andras Stirling <stirling.andras@gmail.com>
2021-07-13 14:00:39 +0200

    Add processor for Nodes in data_mutations_extended

--- src/indra_cogex/sources/cbioportal/__init__.py
+++ src/indra_cogex/sources/cbioportal/__init__.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import os
 
 from pathlib import Path
 from typing import Union
@@ -14,37 +13,65 @@ class CbioportalProcessor(Processor):
 
     name = "cbioportal"
 
-    def __init__(self, path: Union[None, str, Path] = None):
-        if path is None:
-            path = os.path.join(os.path.dirname(__file__), "data_CNA.txt")
-        elif isinstance(path, str):
-            path = Path(path)
+    def __init__(
+        self,
+        cna_path: Union[str, Path, None] = None,
+        mutations_path: Union[str, Path, None] = None,
+    ):
+        if isinstance(cna_path, str):
+            cna_path = Path(cna_path)
 
-        self.df = pd.read_csv(path, sep="\t")
-        self.rel_type = "copy_number_altered_in"
+        if isinstance(mutations_path, str):
+            mutations_path = Path(mutations_path)
+
+        if cna_path is not None:
+            self.cna_df = pd.read_csv(cna_path, sep="\t")
+            self.cna_rel_type = "copy_number_altered_in"
+        else:
+            self.cna_df = None
+
+        if mutations_path is not None:
+            self.mutations_df = pd.read_csv(mutations_path, sep="\t", comment="#")
+            self.mutations_rel_type = "mutated_in"
+        else:
+            self.mutations_df = None
 
     def get_nodes(self):
-        for index, row in self.df.iterrows():
-            hgnc_id = hgnc_client.get_hgnc_id(row["Hugo_Symbol"])
-            if not hgnc_id:
-                continue
-            yield Node(db_ns="hgnc", db_id=hgnc_id, labels=["BioEntity"])
+        if self.cna_df is not None:
+            for index, row in self.cna_df.iterrows():
+                hgnc_id = hgnc_client.get_hgnc_id(row["Hugo_Symbol"])
+                if not hgnc_id:
+                    continue
+                yield Node(db_ns="hgnc", db_id=hgnc_id, labels=["BioEntity"])
 
-        for cell_line in self.df.columns.values[1:]:
-            yield Node(db_ns="ccle", db_id=cell_line, labels=["BioEntity"])
+            for cell_line in self.cna_df.columns.values[1:]:
+                yield Node(db_ns="ccle", db_id=cell_line, labels=["BioEntity"])
+
+        if self.mutations_df is not None:
+            for index, row in self.mutations_df.iterrows():
+                if not pd.isna(row["HGVSp_Short"]):
+                    hgnc_id = hgnc_client.get_hgnc_id(row["Hugo_Symbol"])
+                    cell_line_id = row["Tumor_Sample_Barcode"]
+                    if not hgnc_id:
+                        continue
+                    yield Node(db_ns="hgnc", db_id=hgnc_id, labels=["BioEntity"])
+                    yield Node(db_ns="ccle", db_id=cell_line_id, labels=["BioEntity"])
 
     def get_relations(self):
-        for index, row in self.df.iterrows():
-            hgnc_id = hgnc_client.get_hgnc_id(row["Hugo_Symbol"])
-            if not hgnc_id:
-                continue
-            for cell_line in self.df.columns.values[1:]:
-                if row[cell_line] != 0:
-                    yield Relation(
-                        source_ns="hgnc",
-                        source_id=hgnc_id,
-                        target_ns="ccle",
-                        target_id=cell_line,
-                        rel_type=self.rel_type,
-                        data={"CNA": row[cell_line]},
-                    )
+        if self.cna_df is not None:
+            for index, row in self.cna_df.iterrows():
+                hgnc_id = hgnc_client.get_hgnc_id(row["Hugo_Symbol"])
+                if not hgnc_id:
+                    continue
+                for cell_line in self.cna_df.columns.values[1:]:
+                    if row[cell_line] != 0:
+                        yield Relation(
+                            source_ns="hgnc",
+                            source_id=hgnc_id,
+                            target_ns="ccle",
+                            target_id=cell_line,
+                            rel_type=self.cna_rel_type,
+                            data={"CNA": row[cell_line]},
+                        )
+
+        # TODO: mutation relations (data={"HGVSp_Short": row['HGVSp_Short']})
--- tests/test_cbioportal.py
+++ tests/test_cbioportal.py
@@ -3,29 +3,33 @@ import os
 
 
 def test_get_nodes():
-    path = os.path.join(os.path.dirname(__file__), "test_data_cna.txt")
-    cp = CbioportalProcessor(path)
+    cna_path = os.path.join(os.path.dirname(__file__), "test_data_cna.txt")
+    mutations_path = os.path.join(
+        os.path.dirname(__file__), "test_data_mutations_extended.txt"
+    )
+    cp = CbioportalProcessor(cna_path=cna_path, mutations_path=mutations_path)
     nodes = []
     nodes.extend(cp.get_nodes())
-    assert len(nodes) == 6
-    assert nodes[0].db_id == "A1BG"
-    assert nodes[1].db_id == "NAT2"
-    assert nodes[2].db_id == "ADA"
+    assert len(nodes) == 8
+    assert nodes[0].db_id == "5"
+    assert nodes[1].db_id == "7646"
+    assert nodes[2].db_id == "186"
     assert nodes[3].db_id == "DMS53_LUNG"
     assert nodes[4].db_id == "SW1116_LARGE_INTESTINE"
     assert nodes[5].db_id == "NCIH1694_LUNG"
+    assert nodes[6].db_id == "3084"
+    assert nodes[7].db_id == "127399_SOFT_TISSUE"
 
 
 def test_get_relations():
     path = os.path.join(os.path.dirname(__file__), "test_data_cna.txt")
-    cp = CbioportalProcessor(path)
+    cp = CbioportalProcessor(cna_path=path)
     relations = []
     relations.extend(cp.get_relations())
     assert len(relations) == 2
     assert relations[0].rel_type == "copy_number_altered_in"
-    assert relations[0].source_id == "NAT2"
+    assert relations[0].source_id == "7646"
     assert relations[0].target_id == "DMS53_LUNG"
     assert relations[1].rel_type == "copy_number_altered_in"
-    assert relations[1].source_id == "NAT2"
+    assert relations[1].source_id == "7646"
     assert relations[1].target_id == "SW1116_LARGE_INTESTINE"
-
