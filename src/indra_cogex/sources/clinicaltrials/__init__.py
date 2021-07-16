2a6f221d9f82460c5ca803f4d19516ca4d82a69d
Andras Stirling <stirling.andras@gmail.com>
2021-07-16 13:42:33 +0200

    Add get_relations function and extend get_nodes with interventions

--- src/indra_cogex/sources/clinicaltrials/__init__.py
+++ src/indra_cogex/sources/clinicaltrials/__init__.py
@@ -1,7 +1,10 @@
+import re
 import pandas as pd
 import gilda
 from indra_cogex.sources.processor import Processor
 from indra_cogex.representation import Node, Relation
+
+drug_pattern = re.compile(r"^Drug: ([a-zA-Z ]|\d)+$")
 
 
 class ClinicaltrialsProcessor(Processor):
@@ -20,8 +23,40 @@ class ClinicaltrialsProcessor(Processor):
                         db_ns=matches[0].term.db, db_id=matches[0].term.id, labels=[]
                     )
 
+        for interventions in self.df["Interventions"]:
+            if not pd.isna(interventions):
+                for intervention in interventions.split("|"):
+                    if drug_pattern.match(intervention):
+                        matches = gilda.ground(intervention[6:])
+                        if matches:
+                            yield Node(
+                                db_ns=matches[0].term.db,
+                                db_id=matches[0].term.id,
+                                labels=[],
+                            )
+
     def get_relations(self):
-        # TODO: Replace this placeholder implementation
-        yield Relation(
-            source_ns="a", source_id="1", target_ns="b", target_id="2", rel_type="r"
-        )
+        for conditions in self.df["Conditions"]:
+            for condition in conditions.split("|"):
+                cond_matches = gilda.ground(condition)
+                if cond_matches:
+                    for interventions in self.df["Interventions"]:
+                        if not pd.isna(interventions):
+                            for intervention in interventions.split("|"):
+                                if drug_pattern.match(intervention):
+                                    int_matches = gilda.ground(intervention[6:])
+                                    if int_matches:
+                                        yield Relation(
+                                            source_ns=cond_matches[0].term.db,
+                                            source_id=cond_matches[0].term.id,
+                                            target_ns=int_matches[0].term.db,
+                                            target_id=int_matches[0].term.id,
+                                            rel_type="has_trial",
+                                        )
+                                        yield Relation(
+                                            source_ns=cond_matches[0].term.db,
+                                            source_id=cond_matches[0].term.id,
+                                            target_ns=int_matches[0].term.db,
+                                            target_id=int_matches[0].term.id,
+                                            rel_type="tested_in",
+                                        )
