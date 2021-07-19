a063a58864bd45c9d7464579666bd438830d251d
Andras Stirling <stirling.andras@gmail.com>
2021-07-19 15:58:29 +0200

    Restructure relation generation

--- src/indra_cogex/sources/clinicaltrials/__init__.py
+++ src/indra_cogex/sources/clinicaltrials/__init__.py
@@ -23,8 +23,7 @@ class ClinicaltrialsProcessor(Processor):
                 if matches:
                     valid = True
                     yield Node(
-                        db_ns=matches[0].term.db, db_id=matches[0].term.id,
-                        labels=[]
+                        db_ns=matches[0].term.db, db_id=matches[0].term.id, labels=[]
                     )
 
             if not pd.isna(row["Interventions"]):
@@ -36,41 +35,34 @@ class ClinicaltrialsProcessor(Processor):
                             yield Node(
                                 db_ns=int_matches[0].term.db,
                                 db_id=int_matches[0].term.id,
-                                labels=[]
+                                labels=[],
                             )
 
             if valid:
-                yield Node(
-                    db_ns="CLINICALTRIALS",
-                    db_id=row["URL"][32:],
-                    labels=[]
-                )
+                yield Node(db_ns="CLINICALTRIALS", db_id=row["URL"][32:], labels=[])
 
     def get_relations(self):
         for index, row in self.df.iterrows():
             for condition in row["Conditions"].split("|"):
                 cond_matches = gilda.ground(condition)
                 if cond_matches:
-                    source_ns = cond_matches[0].term.db
-                    source_id = cond_matches[0].term.id
-                    if not pd.isna(row["Interventions"]):
-                        for intervention in row["Interventions"].split("|"):
-                            if drug_pattern.match(intervention):
-                                int_matches = gilda.ground(intervention[6:])
-                                if int_matches:
-                                    target_ns = int_matches[0].term.db
-                                    target_id = row["URL"][32:]
-                                    yield Relation(
-                                        source_ns=source_ns,
-                                        source_id=source_id,
-                                        target_ns=target_ns,
-                                        target_id=target_id,
-                                        rel_type="has_trial",
-                                    )
-                                    yield Relation(
-                                        source_ns=source_ns,
-                                        source_id=source_id,
-                                        target_ns=target_ns,
-                                        target_id=target_id,
-                                        rel_type="tested_in",
-                                    )
+                    yield Relation(
+                        source_ns=cond_matches[0].term.db,
+                        source_id=cond_matches[0].term.id,
+                        target_ns="CLINICALTRIALS",
+                        target_id=row["URL"][32:],
+                        rel_type="has_trial",
+                    )
+
+            if not pd.isna(row["Interventions"]):
+                for intervention in row["Interventions"].split("|"):
+                    if drug_pattern.match(intervention):
+                        int_matches = gilda.ground(intervention[6:])
+                        if int_matches:
+                            yield Relation(
+                                source_ns=int_matches[0].term.db,
+                                source_id=int_matches[0].term.id,
+                                target_ns="CLINICALTRIALS",
+                                target_id=row["URL"][32:],
+                                rel_type="tested_in",
+                            )
