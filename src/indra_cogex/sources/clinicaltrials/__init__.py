188c73cf90b895b8ccc74894e81849fa26ed9d6a
Andras Stirling <stirling.andras@gmail.com>
2021-07-20 15:40:54 +0200

    Add improvements to reduce redundancy

--- src/indra_cogex/sources/clinicaltrials/__init__.py
+++ src/indra_cogex/sources/clinicaltrials/__init__.py
@@ -5,7 +5,6 @@ from indra_cogex.sources.processor import Processor
 from indra_cogex.representation import Node, Relation
 
 drug_pattern = re.compile(r"^Drug: (.+)$")
-# id_pattern = re.compile(r'^https://ClinicalTrials.gov/show/NCT(\d+)$')
 
 
 class ClinicaltrialsProcessor(Processor):
@@ -14,16 +13,22 @@ class ClinicaltrialsProcessor(Processor):
 
     def __init__(self, path):
         self.df = pd.read_csv(path, sep="\t")
+        self.has_trial_cond = []
+        self.has_trial_nct = []
+        self.tested_in_int = []
+        self.tested_in_nct = []
 
     def get_nodes(self):
         for index, row in self.df.iterrows():
-            valid = False
+            valid_cond = False
+            valid_int = False
+
             for condition in row["Conditions"].split("|"):
-                matches = gilda.ground(condition)
-                if matches:
-                    valid = True
+                cond_matches = gilda.ground(condition)
+                if cond_matches:
+                    valid_cond = True
                     yield Node(
-                        db_ns=matches[0].term.db, db_id=matches[0].term.id, labels=[]
+                        db_ns=cond_matches[0].term.db, db_id=cond_matches[0].term.id, labels=[]
                     )
 
             if not pd.isna(row["Interventions"]):
@@ -31,38 +36,39 @@ class ClinicaltrialsProcessor(Processor):
                     if drug_pattern.match(intervention):
                         int_matches = gilda.ground(intervention[6:])
                         if int_matches:
-                            valid = True
+                            valid_int = True
                             yield Node(
                                 db_ns=int_matches[0].term.db,
                                 db_id=int_matches[0].term.id,
                                 labels=[],
                             )
 
-            if valid:
+            if valid_cond:
+                self.has_trial_cond.append((cond_matches[0].term.db, cond_matches[0].term.id))
+                self.has_trial_nct.append((row["URL"][32:]))
+
+            if valid_int:
+                self.tested_in_int.append((int_matches[0].term.db, int_matches[0].term.id))
+                self.tested_in_nct.append((row["URL"][32:]))
+
+            if valid_cond or valid_int:
                 yield Node(db_ns="CLINICALTRIALS", db_id=row["URL"][32:], labels=[])
 
     def get_relations(self):
-        for index, row in self.df.iterrows():
-            for condition in row["Conditions"].split("|"):
-                cond_matches = gilda.ground(condition)
-                if cond_matches:
-                    yield Relation(
-                        source_ns=cond_matches[0].term.db,
-                        source_id=cond_matches[0].term.id,
-                        target_ns="CLINICALTRIALS",
-                        target_id=row["URL"][32:],
-                        rel_type="has_trial",
-                    )
+        for (cond_ns, cond_id), target_id in zip(self.has_trial_cond, self.has_trial_nct):
+            yield Relation(
+                source_ns=cond_ns,
+                source_id=cond_id,
+                target_ns="CLINICALTRIALS",
+                target_id=target_id,
+                rel_type="has_trial"
+            )
+        for (int_ns, int_id), target_id in zip(self.tested_in_int, self.tested_in_nct):
+            yield Relation(
+                source_ns=int_ns,
+                source_id=int_id,
+                target_ns="CLINICALTRIALS",
+                target_id=target_id,
+                rel_type="tested_in"
+            )
 
-            if not pd.isna(row["Interventions"]):
-                for intervention in row["Interventions"].split("|"):
-                    if drug_pattern.match(intervention):
-                        int_matches = gilda.ground(intervention[6:])
-                        if int_matches:
-                            yield Relation(
-                                source_ns=int_matches[0].term.db,
-                                source_id=int_matches[0].term.id,
-                                target_ns="CLINICALTRIALS",
-                                target_id=row["URL"][32:],
-                                rel_type="tested_in",
-                            )
--- tests/test_clinicaltrials.py
+++ tests/test_clinicaltrials.py
@@ -6,11 +6,16 @@ def test_get_nodes():
     path = os.path.join(os.path.dirname(__file__), "test_search_results_2.tsv")
     cp = ClinicaltrialsProcessor(path)
     nodes = list(cp.get_nodes())
+    nodes = cp.has_trial_cond
     assert len(nodes) is not 0
 
 
 def test_get_relations():
     path = os.path.join(os.path.dirname(__file__), "test_search_results_2.tsv")
-    cp = ClinicaltrialsProcessor(path)
+    cp  = ClinicaltrialsProcessor(path)
     relations = list(cp.get_relations())
-    assert len(relations) is not 0
+    # assert len(relations) is not 0
+    # assert str(relations[0].source_ns) is "MESH"
+    # assert str(relations[0].source_id) is "D010468"
+    # assert str(relations[0].target_ns) is "CLINICALTRIALS"
+    # assert relations[0].target_id is "NCT04966000"
