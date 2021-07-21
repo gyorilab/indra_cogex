938335ed97869dc98e96b7192a2e1d6ae244776e
Andras Stirling <stirling.andras@gmail.com>
2021-07-21 12:39:24 +0200

    Fix get_nodes

--- src/indra_cogex/sources/clinicaltrials/__init__.py
+++ src/indra_cogex/sources/clinicaltrials/__init__.py
@@ -8,61 +8,54 @@ drug_pattern = re.compile(r"^Drug: (.+)$")
 
 
 class ClinicaltrialsProcessor(Processor):
-
     name = "clinicaltrials"
 
     def __init__(self, path):
         self.df = pd.read_csv(path, sep="\t")
-        self.has_trial_cond = []
+        self.has_trial_cond_ns = []
+        self.has_trial_cond_id = []
         self.has_trial_nct = []
-        self.tested_in_int = []
+        self.tested_in_int_ns = []
+        self.tested_in_int_id = []
         self.tested_in_nct = []
 
     def get_nodes(self):
         for index, row in self.df.iterrows():
-            valid_cond = False
-            valid_int = False
-
+            valid = False
             for condition in row["Conditions"].split("|"):
                 cond_matches = gilda.ground(condition)
                 if cond_matches:
-                    valid_cond = True
+                    valid = True
                     yield Node(
                         db_ns=cond_matches[0].term.db,
                         db_id=cond_matches[0].term.id,
                         labels=[],
                     )
+                    self.has_trial_cond_ns.append(cond_matches[0].term.db)
+                    self.has_trial_cond_id.append(cond_matches[0].term.id)
+                    self.has_trial_nct.append((row["URL"][32:]))
 
             if not pd.isna(row["Interventions"]):
                 for intervention in row["Interventions"].split("|"):
                     if drug_pattern.match(intervention):
                         int_matches = gilda.ground(intervention[6:])
                         if int_matches:
-                            valid_int = True
+                            valid = True
                             yield Node(
                                 db_ns=int_matches[0].term.db,
                                 db_id=int_matches[0].term.id,
                                 labels=[],
                             )
+                            self.tested_in_int_ns.append(int_matches[0].term.db)
+                            self.tested_in_int_id.append(int_matches[0].term.id)
+                            self.tested_in_nct.append((row["URL"][32:]))
 
-            if valid_cond:
-                self.has_trial_cond.append(
-                    (cond_matches[0].term.db, cond_matches[0].term.id)
-                )
-                self.has_trial_nct.append((row["URL"][32:]))
-
-            if valid_int:
-                self.tested_in_int.append(
-                    (int_matches[0].term.db, int_matches[0].term.id)
-                )
-                self.tested_in_nct.append((row["URL"][32:]))
-
-            if valid_cond or valid_int:
+            if valid:
                 yield Node(db_ns="CLINICALTRIALS", db_id=row["URL"][32:], labels=[])
 
     def get_relations(self):
-        for (cond_ns, cond_id), target_id in zip(
-            self.has_trial_cond, self.has_trial_nct
+        for cond_ns, cond_id, target_id in zip(
+            self.has_trial_cond_ns, self.has_trial_cond_id, self.has_trial_nct
         ):
             yield Relation(
                 source_ns=cond_ns,
@@ -71,7 +64,9 @@ class ClinicaltrialsProcessor(Processor):
                 target_id=target_id,
                 rel_type="has_trial",
             )
-        for (int_ns, int_id), target_id in zip(self.tested_in_int, self.tested_in_nct):
+        for int_ns, int_id, target_id in zip(
+            self.tested_in_int_ns, self.tested_in_int_id, self.tested_in_nct
+        ):
             yield Relation(
                 source_ns=int_ns,
                 source_id=int_id,
--- tests/test_clinicaltrials.py
+++ tests/test_clinicaltrials.py
@@ -6,15 +6,19 @@ def test_get_nodes():
     path = os.path.join(os.path.dirname(__file__), "test_search_results_2.tsv")
     cp = ClinicaltrialsProcessor(path)
     nodes = list(cp.get_nodes())
-    nodes = cp.has_trial_cond
-    assert len(nodes) is not 0
+    assert len(nodes) >= len(cp.has_trial_cond_ns) + len(cp.tested_in_int_ns)
+    assert len(cp.has_trial_cond_ns) is not 0  # 101
+    assert len(cp.has_trial_cond_ns) is len(cp.has_trial_nct)
+    assert len(cp.has_trial_cond_ns) is len(cp.has_trial_cond_id)
+    assert len(cp.tested_in_int_ns) is not 0  # 33
+    assert len(cp.tested_in_int_ns) is len(cp.tested_in_nct)
 
 
 def test_get_relations():
     path = os.path.join(os.path.dirname(__file__), "test_search_results_2.tsv")
-    cp  = ClinicaltrialsProcessor(path)
+    cp = ClinicaltrialsProcessor(path)
     relations = list(cp.get_relations())
-    # assert len(relations) is not 0
+    assert len(relations) is not 0
     # assert str(relations[0].source_ns) is "MESH"
     # assert str(relations[0].source_id) is "D010468"
     # assert str(relations[0].target_ns) is "CLINICALTRIALS"
