f03474dbb0897b2b085627af1f4c091aad5527e1
Ben Gyori <ben.gyori@gmail.com>
2021-07-29 23:15:38 -0400

    Improve condition grounding and simplify

--- src/indra_cogex/sources/clinicaltrials/__init__.py
+++ src/indra_cogex/sources/clinicaltrials/__init__.py
@@ -16,53 +16,54 @@ class ClinicaltrialsProcessor(Processor):
         self.tested_in_int_id = []
         self.tested_in_nct = []
 
+    def ground_condition(self, condition):
+        matches = gilda.ground(condition)
+        matches = [
+            match
+            for match in matches
+            if match.term.db in {"MESH", "DOID", "EFO", "HP", "GO"}
+        ]
+        if matches:
+            return matches[0].term
+        return None
+
+    def ground_drug(self, drug):
+        matches = gilda.ground(drug)
+        if matches:
+            return matches[0].term
+        return None
+
     def get_nodes(self):
         for index, row in self.df.iterrows():
             for condition in str(row["Condition"]).split("|"):
-                cond_matches = gilda.ground(condition)
-                if cond_matches:
-                    self.has_trial_cond_ns.append(cond_matches[0].term.db)
-                    self.has_trial_cond_id.append(cond_matches[0].term.id)
-                    self.has_trial_nct.append(row["NCTId"])
-                    yield Node(
-                        db_ns=cond_matches[0].term.db,
-                        db_id=cond_matches[0].term.id,
-                        labels=[],
-                    )
+                cond_term = self.ground_condition(condition)
+                if cond_term:
+                    cond_ns = cond_term.db
+                    cond_id = cond_term.id
                 else:
-                    self.has_trial_cond_ns.append("MESH")
-                    self.has_trial_cond_id.append(row["ConditionMeshId"])
-                    self.has_trial_nct.append(row["NCTId"])
-                    yield Node(
-                        db_ns="MESH",
-                        db_id=row["ConditionMeshId"],
-                        labels=[],
-                    )
+                    cond_ns = "MESH"
+                    cond_id = row["ConditionMeshId"]
+                self.has_trial_cond_ns.append(cond_ns)
+                self.has_trial_cond_id.append(cond_id)
+                self.has_trial_nct.append(row["NCTId"])
+                yield Node(db_ns=cond_ns, db_id=cond_id, labels=["BioEntity"])
 
             for int_name, int_type in zip(
                 str(row["InterventionName"]).split("|"),
                 str(row["InterventionType"]).split("|"),
             ):
                 if int_type == "Drug":
-                    int_matches = gilda.ground(int_name)
-                    if int_matches:
-                        self.tested_in_int_ns.append(int_matches[0].term.db)
-                        self.tested_in_int_id.append(int_matches[0].term.id)
-                        self.tested_in_nct.append(row["NCTId"])
-                        yield Node(
-                            db_ns=int_matches[0].term.db,
-                            db_id=int_matches[0].term.id,
-                            labels=[],
-                        )
+                    drug_term = self.ground_drug(int_name)
+                    if drug_term:
+                        drug_ns = drug_term.db
+                        drug_id = drug_term.id
                     else:
-                        self.tested_in_int_ns.append("MESH")
-                        self.tested_in_int_id.append(row["InterventionMeshTerm"])
-                        self.tested_in_nct.append(row["NCTId"])
-                        yield Node(
-                            db_ns="MESH",
-                            db_id=row["InterventionMeshTerm"],
-                            labels=[],
-                        )
+                        drug_ns = "MESH"
+                        drug_id = row["InterventionMeshTerm"]
+                    self.tested_in_int_ns.append(drug_ns)
+                    self.tested_in_int_id.append(drug_id)
+                    self.tested_in_nct.append(row["NCTId"])
+                    yield Node(db_ns=drug_ns, db_id=drug_id, labels=["BioEntity"])
 
             yield Node(db_ns="CLINICALTRIALS", db_id=row["NCTId"], labels=[])
 
