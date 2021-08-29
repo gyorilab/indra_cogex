88447dc92d558c5a755864f7655b6810fcfd362a
Ben Gyori <ben.gyori@gmail.com>
2021-08-29 14:52:04 -0400

    Improve interpretation graph generation

--- src/indra_cogex/sources/odinson/document.py
+++ src/indra_cogex/sources/odinson/document.py
@@ -2,8 +2,12 @@
 interpretations and for processing into this representation as well
 as generating visualizations."""
 
+import glob
 import gzip
 import json
+import tqdm
+import os.path
+
 import networkx
 
 
@@ -41,6 +45,8 @@ class Sentence:
         ntokens = sentence_data["numTokens"]
         fields = sentence_data["fields"]
         token_data = [{} for _ in range(ntokens)]
+        roots = None
+        edges = None
         for field in fields:
             if field["$type"] == "ai.lum.odinson.TokensField":
                 for idx in range(ntokens):
@@ -49,10 +55,14 @@ class Sentence:
                 edges = field["edges"]
                 roots = field["roots"]
         self.tokens = [Token(**td) for td in token_data]
-        self.dependency_graph = make_graph(self.tokens, roots, edges)
+        if not roots and not edges:
+            self.dependency_graph = None
+        else:
+            self.dependency_graph = make_graph(self.tokens, roots, edges)
 
     def draw_graph(self, fname):
-        return draw_graph(self.dependency_graph, fname)
+        if self.dependency_graph:
+            return draw_graph(self.dependency_graph, fname)
 
     def __str__(self):
         return "Sentence(%s)" % ", ".join([t.word for t in self.tokens])
@@ -70,6 +80,8 @@ class Document:
     def draw_graph(self, fname):
         graphs = []
         for idx, sentence in enumerate(self.sentences):
+            if not sentence.dependency_graph:
+                continue
             g = networkx.relabel_nodes(
                 sentence.dependency_graph,
                 mapping={
@@ -108,6 +120,10 @@ def make_graph(tokens, roots, edges):
 
 
 def draw_graph(g, fname):
+    g = g.copy()
+    for node in g.nodes:
+        if "\\" in g.nodes[node]["label"]:
+            g.nodes[node]["label"] = g.nodes[node]["label"].replace("\\", "[backslash]")
     ag = networkx.nx_agraph.to_agraph(g)
     # Add some visual styles to the graph
     ag.node_attr["shape"] = "plaintext"
@@ -115,6 +131,19 @@ def draw_graph(g, fname):
     ag.graph_attr["rankdir"] = "TD"
     ag.draw(fname, prog="dot")
     return ag
+
+
+def process_into_graphs(docs_path, cached=True):
+    fnames = glob.glob(os.path.join(docs_path, "*.json.gz"))
+    for fname in tqdm.tqdm(fnames):
+        doc = process_document(fname)
+        out_fname = os.path.join(docs_path, "%s.pdf" % doc.doc_id)
+        if cached and os.path.exists(out_fname):
+            continue
+        try:
+            doc.draw_graph(out_fname)
+        except Exception as e:
+            continue
 
 
 def process_document(json_gz_path):
