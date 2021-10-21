97f8eb410c9ed886f1726d3fbecc2b33a5dcf445
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-10-21 16:12:09 +0200

    Add SIDER processor

--- src/indra_cogex/sources/__init__.py
+++ src/indra_cogex/sources/__init__.py
@@ -5,17 +5,19 @@
 from class_resolver import Resolver
 
 from .bgee import BgeeProcessor
+from .cbioportal import (
+    CcleCnaProcessor,
+    CcleDrugResponseProcessor,
+    CcleMutationsProcessor,
+)
+from .chembl import ChemblIndicationsProcessor
+from .clinicaltrials import ClinicaltrialsProcessor
 from .goa import GoaProcessor
 from .indra_db import DbProcessor
 from .indra_ontology import OntologyProcessor
 from .pathways import ReactomeProcessor, WikipathwaysProcessor
-from .cbioportal import (
-    CcleCnaProcessor,
-    CcleMutationsProcessor,
-    CcleDrugResponseProcessor,
-)
-from .clinicaltrials import ClinicaltrialsProcessor
 from .processor import Processor
+from .sider import SIDERSideEffectProcessor
 
 __all__ = [
     "processor_resolver",
@@ -30,6 +32,8 @@ __all__ = [
     "CcleMutationsProcessor",
     "CcleDrugResponseProcessor",
     "ClinicaltrialsProcessor",
+    "ChemblIndicationsProcessor",
+    "SIDERSideEffectProcessor",
 ]
 
 processor_resolver = Resolver.from_subclasses(Processor)
