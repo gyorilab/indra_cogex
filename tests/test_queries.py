e21de02ee2089764e513eeb14f3a1470709dafab
kkaris <karis.klas@gmail.com>
2022-01-06 09:55:45 -0500

    Start tests for query module

---
+++ tests/test_queries.py
@@ -0,0 +1,26 @@
+import pytest
+from indra_cogex.client.queries import *
+from indra.statements import Inhibition
+
+from .test_neo4j_client import _get_client
+
+
+@pytest.mark.nonpublic
+def test_get_pmids_for_mesh():
+    # Single query
+    client = _get_client()
+    pmids = get_pmids_for_mesh(client, ('MESH', 'D015002'))
+    assert len(pmids) == 1591
+    assert pmids[0].startswith('pubmed:')
+
+
+@pytest.mark.nonpublic
+def test_get_stmts_by_hashes():
+    # Note This statement has a 100s of evidences
+    # Two queries: first statements, then all the evidence for the statements
+    stmt_hashes = ['35279776755000170']
+    client = _get_client()
+    stmts = get_stmts_for_stmt_hashes(client, stmt_hashes)
+    assert len(stmts) == 1
+    assert isinstance(stmts[0], Inhibition)
+
