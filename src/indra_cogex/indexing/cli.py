7fca7a1021d29edc64daf4c88b9fab448b9a7c54
kkaris <karis.klas@gmail.com>
2022-01-11 13:01:49 -0500

    Add relationship indexing on indra_rel to cli

--- src/indra_cogex/indexing/cli.py
+++ src/indra_cogex/indexing/cli.py
@@ -9,18 +9,31 @@ import click
 @click.option(
     "--index-evidence-nodes",
     is_flag=True,
-    help="Index the Evidence nodes on the stmt_hash property."
+    help="Index the Evidence nodes on the stmt_hash property.",
+)
+@click.option(
+    "--index-indra-relations",
+    is_flag=True,
+    help="Index the INDRA relations on the stmt_hash property.",
 )
 @click.option(
     "--exist-ok",
     is_flag=True,
     help="If set, skip already set indices silently, otherwise an exception "
-         "is raised if attempting to set an index that already exists."
+    "is raised if attempting to set an index that already exists.",
 )
-def main(index_evidence_nodes: bool = False, exist_ok: bool = False):
+def main(
+    index_evidence_nodes: bool = False,
+    index_indra_relations: bool = False,
+    exist_ok: bool = False,
+):
     """Build indexes on the database."""
     if index_evidence_nodes:
         from . import index_evidence_on_stmt_hash
         click.secho("Indexing Evidence nodes on the stmt_hash property.", fg="green")
         index_evidence_on_stmt_hash(exist_ok=exist_ok)
+    if index_indra_relations:
+        from . import index_indra_rel_on_stmt_hash
+        click.secho("Indexing INDRA relations on the stmt_hash property.", fg="green")
+        index_indra_rel_on_stmt_hash()
     click.secho("Started all requested indexing.", fg="green")
