e062301125059cf09755bf19156cbebcfb289699
kkaris <karis.klas@gmail.com>
2022-01-11 14:18:36 -0500

    Add --all option to build all available indexes

--- src/indra_cogex/indexing/cli.py
+++ src/indra_cogex/indexing/cli.py
@@ -8,6 +8,12 @@ import click
 
 @click.command(name="Build extra indexes")
 @click.option(
+    "--all",
+    "all_",
+    is_flag=True,
+    help="Build all indexes",
+)
+@click.option(
     "--index-evidence-nodes",
     is_flag=True,
     help="Index the Evidence nodes on the stmt_hash property.",
@@ -24,6 +30,7 @@ import click
     "is raised if attempting to set an index that already exists.",
 )
 def main(
+    all_: bool = False,
     index_evidence_nodes: bool = False,
     index_indra_relations: bool = False,
     exist_ok: bool = False,
@@ -32,11 +39,11 @@ def main(
 ):
     """Build indexes on the database."""
     client = _get_client(url, auth)
-    if index_evidence_nodes:
+    if all_ or index_evidence_nodes:
         from . import index_evidence_on_stmt_hash
         click.secho("Indexing Evidence nodes on the stmt_hash property.", fg="green")
         index_evidence_on_stmt_hash(client, exist_ok=exist_ok)
-    if index_indra_relations:
+    if all_ or index_indra_relations:
         from . import index_indra_rel_on_stmt_hash
         click.secho("Indexing INDRA relations on the stmt_hash property.", fg="green")
         index_indra_rel_on_stmt_hash(client)
