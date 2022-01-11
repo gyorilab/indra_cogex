eeca7b2f4a4abcb17918f4b104479e801aa2bed4
kkaris <karis.klas@gmail.com>
2022-01-11 13:11:33 -0500

    Use one client for all indexing functions in cli

--- src/indra_cogex/indexing/cli.py
+++ src/indra_cogex/indexing/cli.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Run the database indexing."""
+from typing import Optional, Tuple
 
 import click
 
@@ -26,14 +27,25 @@ def main(
     index_evidence_nodes: bool = False,
     index_indra_relations: bool = False,
     exist_ok: bool = False,
+    url: Optional[str] = None,
+    auth: Optional[Tuple[str, str]] = None,
 ):
     """Build indexes on the database."""
+    client = _get_client(url, auth)
     if index_evidence_nodes:
         from . import index_evidence_on_stmt_hash
         click.secho("Indexing Evidence nodes on the stmt_hash property.", fg="green")
-        index_evidence_on_stmt_hash(exist_ok=exist_ok)
+        index_evidence_on_stmt_hash(client, exist_ok=exist_ok)
     if index_indra_relations:
         from . import index_indra_rel_on_stmt_hash
         click.secho("Indexing INDRA relations on the stmt_hash property.", fg="green")
-        index_indra_rel_on_stmt_hash()
+        index_indra_rel_on_stmt_hash(client)
     click.secho("Started all requested indexing.", fg="green")
+
+
+def _get_client(
+    url: Optional[str] = None, auth: Optional[Tuple[str, str]] = None
+) -> "Neo4jClient":
+    from ..client.neo4j_client import Neo4jClient
+
+    return Neo4jClient(url, auth)
