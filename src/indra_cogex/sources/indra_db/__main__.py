9c2a6fd5c8af1d06e4c6d2e3fcb56b59c41a6606
kkaris <karis.klas@gmail.com>
2022-02-15 14:03:11 -0500

    Make both processors runnable with click

--- src/indra_cogex/sources/indra_db/__main__.py
+++ src/indra_cogex/sources/indra_db/__main__.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 
 """Run the INDRA database processor using ``python -m indra_cogex.sources.indra_db``."""
+import click
+from more_click import verbose_option
 
 from . import DbProcessor, EvidenceProcessor
 
+
+@click.command()
+@verbose_option
+@click.pass_context
+def _main(ctx: click.Context):
+    ctx.invoke(DbProcessor.get_cli())
+    ctx.invoke(EvidenceProcessor.get_cli())
+
+
 if __name__ == "__main__":
-    DbProcessor.cli()
-    EvidenceProcessor.cli()
+    _main()
