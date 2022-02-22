69c83403684b3bab40e1db55ef5035f484ff6ebb
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-02-22 14:13:18 +0100

    Update constants.py

--- src/indra_cogex/apps/gla/constants.py
+++ src/indra_cogex/apps/gla/constants.py
@@ -6,4 +6,4 @@ __all__ = [
     "INDRA_COGEX_WEB_LOCAL",
 ]
 
-INDRA_COGEX_WEB_LOCAL = os.environ.get("INDRA_COGEX_WEB_LOCAL")
+INDRA_COGEX_WEB_LOCAL = os.environ.get("INDRA_COGEX_WEB_LOCAL", "").lower() in {"t", "true"}
