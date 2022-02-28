6a305a07819e064281d6f49467f8a02dfafc63b3
kkaris <karis.klas@gmail.com>
2022-02-28 16:29:49 -0500

    Add examples. Run black.

--- src/indra_cogex/apps/query_web_app/__init__.py
+++ src/indra_cogex/apps/query_web_app/__init__.py
@@ -3,8 +3,7 @@
 """An app wrapping the query module of indra_cogex."""
 import logging
 from inspect import isfunction, signature, Signature
-from typing import Callable, Tuple, Type, Iterable, Any, Dict, List, Counter, \
-    Mapping
+from typing import Callable, Tuple, Type, Iterable, Any, Dict, List, Counter, Mapping
 
 import flask
 from docstring_parser import parse
@@ -27,6 +26,30 @@ client = Neo4jClient()
 
 
 logger = logging.getLogger(__name__)
+
+
+examples_dict = {
+    "tissue": ["UBERON", "UBERON:0002349"],
+    "gene": ["HGNC", "9896"],
+    "go_term": ["GO", "GO:0000978"],
+    "drug": ["CHEBI", "CHEBI:27690"],
+    "disease": ["MESH", "D007855"],
+    "trial": ["CLINICALTRIALS", "NCT00000114"],
+    "genes": [["HGNC", "1097"], ["HGNC", "6407"]],
+    "pathway": ["WIKIPATHWAYS", "WP5037"],
+    "side_effect": ["UMLS", "C3267206"],
+    "term": ["MESH", "D007855"],
+    "parent": ["MESH", "D007855"],
+    "mesh_term": ["MESH", "D015002"],
+    "pmid_term": ["PUBMED", "27890007"],
+    "include_child_terms": True,
+    "stmt_hash": 12198579805553967,
+    "stmt_hashes": [12198579805553967, 30651649296901235],
+    "cell_line": ["CCLE", "BT20_BREAST"],
+    "target": ["HGNC", "6840"],
+    "include_indirect": True,
+    "evidence_map": {}
+}
 
 
 def process_result(result) -> Any:
@@ -106,7 +129,7 @@ Returns
     param_list = []
     for param in parsed_doc.params:
         # Skip client, evidence_map,
-        if param.arg_name in ("client", 'evidence_map'):
+        if param.arg_name in ("client", "evidence_map"):
             continue
 
         str_type = str(sig.parameters[param.arg_name].annotation).replace("typing.", "")
@@ -154,15 +177,24 @@ for func_name in queries.__all__:
     if len(func_sig.parameters) == 2:
         # Get the parameters name for the other parameter that is not 'client'
         query_model = api.model(
-            model_name, {param_names[0]: fields.List(fields.String)}
+            model_name,
+            {
+                param_names[0]: fields.List(
+                    fields.String, example=examples_dict[param_names[0]]
+                )
+            },
         )
     elif len(func_sig.parameters) == 3:
 
         query_model = api.model(
             model_name,
             {
-                param_names[0]: fields.List(fields.String),
-                param_names[1]: fields.List(fields.String),
+                param_names[0]: fields.List(
+                    fields.String, example=examples_dict[param_names[0]]
+                ),
+                param_names[1]: fields.List(
+                    fields.String, example=examples_dict[param_names[1]]
+                ),
             },
         )
     else:
