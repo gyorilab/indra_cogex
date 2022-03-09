0634e8426170941aaf8a291bc27e59667b0977b4
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-03-09 16:07:28 +0100

    Add curation workflow

---
+++ src/indra_cogex/client/curation.py
@@ -0,0 +1,72 @@
+"""Tools for INDRA curation."""
+
+from typing import Iterable, List, Optional
+
+import pandas as pd
+from indra.assemblers.indranet import IndraNetAssembler
+from indra.statements import Statement
+from networkx.algorithms import edge_betweenness_centrality
+
+__all__ = [
+    "get_prioritized_stmt_hashes",
+    "get_curation_df",
+]
+
+# TODO can this be imported from INDRA and auto-generated?
+DATABASES = {"biogrid", "hprd", 'signor', 'phosphoelm', 'signor', 'biopax'}
+
+
+def _keep_by_source(source_counts) -> bool:
+    return all(k not in DATABASES for k in source_counts)
+
+
+def _get_text(stmt: Statement) -> Optional[str]:
+    return stmt.evidence[0].text if stmt.evidence else None
+
+
+def _get_curated_statement_hashes() -> set[int]:
+    return set()  # FIXME implement!
+
+
+def get_prioritized_stmt_hashes(stmts: Iterable[Statement]) -> List[int]:
+    """Get the priority ordered hashes of statements to curate from."""
+    df = get_curation_df(stmts)
+    return list(df["stmt_hash"])
+
+
+def get_curation_df(stmts: Iterable[Statement]) -> pd.DataFrame:
+    """Generate a curation dataframe from INDRA statements."""
+    assembler = IndraNetAssembler(list(stmts))
+
+    # Get centrality measurements that are unaffected by other filters
+    centralities = edge_betweenness_centrality(assembler.make_model())
+
+    # Make the dataframe but include an extra column for text.
+    # This works since the INDRANet assembler currently goes one
+    # evidence per row (i.e., a statement could have many rows)
+    df = assembler.make_df(extra_columns=[("text", _get_text)])
+
+    # Don't worry about curating statements with no text
+    df = df[df.text.notna()]
+
+    # Don't worry about curating statements that are already curated
+    curated_statement_hashes = _get_curated_statement_hashes()
+    df = df[~df["stmt_hash"].isin(curated_statement_hashes)]
+
+    # Don't worry about curating statements
+    # that already have database evidence
+    df = df[df["source_counts"].map(_keep_by_source)]
+
+    # Look up centralities for remaining rows
+    df["centralities"] = [
+        centralities.get((left, right))
+        for left, right in df[["agA_name", "agB_name"]].values
+    ]
+
+    # Sort by the most central edges
+    df = df.sort_values("centralities", ascending=False)
+
+    # If several evidences from the same statement, dont need
+    # to have the hash multiple times
+    df = df.drop_duplicates("stmt_hash")
+    return df
