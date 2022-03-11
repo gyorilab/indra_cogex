8871b14b9051a8b50153b83bc2d09ea57551e052
kkaris <karis.klas@gmail.com>
2022-03-11 12:54:41 -0500

    Enable gla app to be run standalone

--- src/indra_cogex/apps/gla/wsgi.py
+++ src/indra_cogex/apps/gla/wsgi.py
@@ -3,20 +3,42 @@
 """An app for gene list analysis."""
 
 import os
+from pathlib import Path
 
+from flask import Flask
+from flask_bootstrap import Bootstrap4
 from more_click import make_web_command
+
+from indra_cogex.apps.proxies import INDRA_COGEX_EXTENSION
+from indralab_auth_tools.auth import config_auth, auth
 
 from .gene_blueprint import gene_blueprint
 from .metabolite_blueprint import metabolite_blueprint
-from .. import get_flask_app
+from ...client.neo4j_client import Neo4jClient
 
-app = get_flask_app(__name__)
+APPS_DIR = Path(__file__).parent.parent.absolute()
+TEMPLATES_DIR = APPS_DIR / "templates"
+STATIC_DIR = APPS_DIR / "static"
+
+app = Flask(__name__, template_folder=str(TEMPLATES_DIR), static_folder=STATIC_DIR)
+
+if not hasattr(app, "extensions"):
+    print("no extensions?")
+    app.extensions = {}
+
+bootstrap = Bootstrap4(app)
+
+app.extensions[INDRA_COGEX_EXTENSION] = Neo4jClient()
+
+SC, jwt = config_auth(app)
 
 # Secret key must be set to use flask-wtf, but there's no *really*
 # secure information in this app so it's okay to set randomly
 app.config["WTF_CSRF_ENABLED"] = False
 app.config["SECRET_KEY"] = os.urandom(8)
 
+# Register blueprints
+app.register_blueprint(auth)
 app.register_blueprint(gene_blueprint)
 app.register_blueprint(metabolite_blueprint)
 
