d84b461cb106029ad40359fa914d3f5361ee9e9f
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-03-29 13:48:53 +0200

    Update curation summary generation

--- src/indra_cogex/apps/curator/utils.py
+++ src/indra_cogex/apps/curator/utils.py
@@ -1,7 +1,7 @@
 """Utilities for the curation blueprint."""
 
 from collections import Counter, defaultdict
-from typing import Any, List, Mapping
+from typing import Any, Iterable, List, Mapping, Tuple
 
 from indra.sources.indra_db_rest import get_curations
 
@@ -11,6 +11,7 @@ __all__ = [
     "get_conflict_evidence_counts",
     "unfinished",
     "Curation",
+    "iterate_conflicts",
 ]
 
 Curation = Mapping[str, Any]
@@ -21,6 +22,20 @@ def get_conflict_evidence_counts(
     *, curations: List[Curation] = None, client: Neo4jClient
 ) -> Mapping[int, int]:
     """Get hashes of statements whose curations need resolving."""
+    return {
+        stmt_hash: evidence_count
+        for stmt_hash, evidence_count, status in iterate_conflicts(
+            curations=curations, client=client
+        )
+        if status
+    }
+
+
+@autoclient()
+def iterate_conflicts(
+    *, curations: List[Curation] = None, client: Neo4jClient
+) -> Iterable[Tuple[int, int, bool]]:
+    """Iterate hashes of statements and their resolution status."""
     if curations is None:
         curations = get_curations()
     stmt_hash_to_counter = _group_curations(curations)
@@ -30,15 +45,12 @@ def get_conflict_evidence_counts(
             r.stmt_hash IN {sorted(stmt_hash_to_counter)!r}
         RETURN r.stmt_hash, r.evidence_count
     """
-    return {
-        stmt_hash: evidence_count
-        for stmt_hash, evidence_count in client.query_tx(query)
-        if unfinished(
+    for stmt_hash, evidence_count in client.query_tx(query):
+        yield stmt_hash, evidence_count, unfinished(
             correct=stmt_hash_to_counter[stmt_hash][True],
             incorrect=stmt_hash_to_counter[stmt_hash][False],
             evidences=evidence_count,
         )
-    }
 
 
 def _group_curations(curations: List[Curation]) -> Mapping[int, Counter]:
@@ -52,7 +64,9 @@ def _group_curations(curations: List[Curation]) -> Mapping[int, Counter]:
     return dict(stmt_hash_to_counter)
 
 
-def unfinished(correct: int, incorrect: int, evidences: int) -> bool:
+def unfinished(
+    correct: int, incorrect: int, evidences: int, threshold: int = 5
+) -> bool:
     """Decide if curation should continue on a given statement
     based on previous curation counts.
 
@@ -66,6 +80,9 @@ def unfinished(correct: int, incorrect: int, evidences: int) -> bool:
         previously curated as incorrect
     evidences :
         The number of evidences for the statement
+    threshold:
+        The threshold for incorrect evidences in absence of correct evidences
+        for marking a statement as incorrect
 
     Returns
     -------
@@ -81,7 +98,7 @@ def unfinished(correct: int, incorrect: int, evidences: int) -> bool:
     if incorrect == 0 and correct > 0:
         # At least one evidence has been curated as correct and none as incorrect
         return False
-    if correct == 0 and incorrect > 5:
+    if correct == 0 and incorrect > threshold:
         # Several evidences have been curated as incorrect and none as correct
         # At a certain point, it's better to just stop curating.
         return False
