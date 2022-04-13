311d7397d29d1c75854a291aed4bf63f049f6279
kkaris <karis.klas@gmail.com>
2022-04-13 14:11:34 -0400

    Move chat area to middle of page

--- src/indra_cogex/apps/static/chat/js/app.js
+++ src/indra_cogex/apps/static/chat/js/app.js
@@ -18,7 +18,7 @@
 
     const chatPage   = $(document)
     const chatWindow = $('.chatbubble')
-    const chatHeader = chatWindow.find('.unexpanded')
+    const chatHeader = chatWindow.find('.chat-title')
     const chatBody   = chatWindow.find('.chat-window')
 
 
@@ -28,16 +28,6 @@
 
     let helpers = {
 
-    // ----------------------------------------------------
-    // Toggles the display of the chat window.
-    // ----------------------------------------------------
-
-        ToggleChatWindow: function () {
-            chatWindow.toggleClass('opened')
-            chatHeader.find('.title').text(
-                chatWindow.hasClass('opened') ? 'Minimize Chat Window' : 'Chat with INDRA CoGEx'
-            )
-        },
     // --------------------------------------------------------------------
     // Show the appropriate display screen. Login screen or Chat screen.
     // --------------------------------------------------------------------
@@ -144,7 +134,7 @@
 
             evt.preventDefault()
         }
-    }
+    } // End of helpers object.
 
     // ------------------------------------------------------------------
     // Listen for a new message event from the admin
@@ -160,7 +150,7 @@
     // ----------------------------------------------------
 
     chatPage.ready(helpers.ShowAppropriateChatDisplay)
-    chatHeader.on('click', helpers.ToggleChatWindow)
+    chatHeader.on('click', helpers.ToggleChatWindow) // Toggle chat window
     chatBody.find('#loginScreenForm').on('submit', helpers.LogIntoChatSession)
     chatBody.find('#messageSupport').on('submit', helpers.SendMessageToSupport)
 }())
