17ad23772dbad2471695658e417c43325ef3fb00
kkaris <karis.klas@gmail.com>
2022-04-19 17:00:03 -0400

    Add new component fetching app key on mount

---
+++ src/indra_cogex/apps/chat_page/app/src/components/DiscoveryApp.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>This is the app key: {{ app_key }}</p>
+</template>
+
+<script>
+export default {
+  name: "DiscoveryApp.vue",
+  data() {
+    return {
+      app_key: "",
+    };
+  },
+  mounted() {
+    this.getAppKey();
+  },
+  methods: {
+    async getAppKey() {
+      const resp = await fetch("/chat/pusher_info");
+      const pusher_info = await resp.json();
+      this.app_key = pusher_info.pusher_app_key;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
