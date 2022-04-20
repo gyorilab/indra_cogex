b98d3a09df7a183b2ece7937f0355745cb29d80b
kkaris <karis.klas@gmail.com>
2022-04-20 11:46:08 -0400

    Await getAppKey in mounted

--- src/indra_cogex/apps/chat_page/app/src/components/DiscoveryApp.vue
+++ src/indra_cogex/apps/chat_page/app/src/components/DiscoveryApp.vue
@@ -10,8 +10,8 @@ export default {
       app_key: "",
     };
   },
-  mounted() {
-    this.getAppKey();
+  async mounted() {
+    await this.getAppKey();
   },
   methods: {
     async getAppKey() {
