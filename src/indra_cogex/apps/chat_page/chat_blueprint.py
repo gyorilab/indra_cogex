4f1d337efe8bbfe5d60afff5b09798591b0afb52
kkaris <karis.klas@gmail.com>
2022-04-20 11:44:29 -0400

    Send empty string if pusher app key is not defined

--- src/indra_cogex/apps/chat_page/chat_blueprint.py
+++ src/indra_cogex/apps/chat_page/chat_blueprint.py
@@ -38,7 +38,7 @@ else:
 # Return simple json with pusher app key
 @chat_blueprint.route("/pusher_info", methods=["GET"])
 def pusher_info():
-    return json.dumps({"pusher_app_key": pusher_key})
+    return json.dumps({"pusher_app_key": pusher_key or ""})
 
 
 @chat_blueprint.route("/")
