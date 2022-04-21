2d48338f443d56e0733f5032817b882f04ec3a0b
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-04-21 12:30:29 +0200

    Improve processing of curations
    1. add process_curation hook, that currently uses dateutil to parse the times
    2. externalize the dataframe processing into its own function

--- tests/test_curation_cache.py
+++ tests/test_curation_cache.py
@@ -6,19 +6,24 @@ from indra_cogex.apps.curator.curation_cache import CurationCache, Curations
 class MockCurationCache(CurationCache):
     def __init__(self, curations: Curations):
         super().__init__()
-        self.curation_list.extend(curations)
+        self.curation_list.extend(
+            self._process_curation(curation) for curation in curations
+        )
 
     def refresh_curations(self):
         pass
 
 
+TEST_DATE = "Thu, 29 Nov 2018 18:00:08 GMT"
+
+
 class TestCurationCache(unittest.TestCase):
     def test_get_incorrect_evidence_hashes(self):
         curations = [
-            dict(source_hash=1, tag="correct"),
-            dict(source_hash=1, tag="incorrect"),
-            dict(source_hash=2, tag="correct"),
-            dict(source_hash=3, tag="incorrect"),
+            dict(source_hash=1, tag="correct", date=TEST_DATE),
+            dict(source_hash=1, tag="incorrect", date=TEST_DATE),
+            dict(source_hash=2, tag="correct", date=TEST_DATE),
+            dict(source_hash=3, tag="incorrect", date=TEST_DATE),
         ]
         curation_cache = MockCurationCache(curations)
         self.assertEqual({3}, curation_cache.get_incorrect_evidence_hashes())
@@ -27,10 +32,10 @@ class TestCurationCache(unittest.TestCase):
 
     def test_get_correct_statement_hashes(self):
         curations = [
-            dict(pa_hash=1, tag="correct"),
-            dict(pa_hash=1, tag="incorrect"),
-            dict(pa_hash=2, tag="correct"),
-            dict(pa_hash=3, tag="incorrect"),
+            dict(pa_hash=1, tag="correct", date=TEST_DATE),
+            dict(pa_hash=1, tag="incorrect", date=TEST_DATE),
+            dict(pa_hash=2, tag="correct", date=TEST_DATE),
+            dict(pa_hash=3, tag="incorrect", date=TEST_DATE),
         ]
         curation_cache = MockCurationCache(curations)
         statement_hashes = curation_cache.get_correct_statement_hashes()
