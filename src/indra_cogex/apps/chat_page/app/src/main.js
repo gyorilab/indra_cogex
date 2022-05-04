17478686967b1a6dd59deb3c040885a7113e153f
kkaris <karis.klas@gmail.com>
2022-05-04 15:56:18 -0400

    Popper and bootstrap imports seem to work

--- src/indra_cogex/apps/chat_page/app/src/main.js
+++ src/indra_cogex/apps/chat_page/app/src/main.js
@@ -1,4 +1,5 @@
-import "bootstrap";
+import "@popperjs/core/dist/esm/popper.js";
+import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { createApp } from "vue";
