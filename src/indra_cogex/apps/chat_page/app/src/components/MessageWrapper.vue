8097f08052f6b380638854e793e4b027cb97711a
kkaris <karis.klas@gmail.com>
2022-05-04 15:29:22 -0400

    Update MessageWrapper.vue

--- src/indra_cogex/apps/chat_page/app/src/components/MessageWrapper.vue
+++ src/indra_cogex/apps/chat_page/app/src/components/MessageWrapper.vue
@@ -7,7 +7,7 @@
             >User Input</span
           >
         </div>
-        <div class="col-11 text-left">
+        <div class="col-11 text-start">
           <span>{{ user_message.input }}</span>
         </div>
       </div>
@@ -18,17 +18,35 @@
             >Output ({{ shortDate }})
           </span>
         </div>
-        <div class="col-11 text-left">
+        <div class="col-11 text-start">
           <span v-html="message.text"></span>
         </div>
       </div>
+      <template v-if="entityList.length">
+        <hr />
+        <div class="row">
+          <div class="col-1">
+            <span class="text-muted small" :title="receivedDate"
+              >Entity list ({{ entityList.length }})
+            </span>
+          </div>
+          <div class="col-11 text-start">
+            <EntityList :entities="entityList" />
+          </div>
+        </div>
+      </template>
     </div>
   </div>
 </template>
 
 <script>
+import EntityList from "@/components/EntityList.vue";
+
 export default {
   name: "MessageWrapper.vue",
+  components: {
+    EntityList,
+  },
   data() {
     return {
       listMax: 0,
@@ -40,6 +58,7 @@ export default {
        * {
        *  text: "",
        *  name: "",
+       *  entities: [{gnd: "", nm: ""}, ...],
        *  sender: "",
        *  receivedAt: "",
        * }
@@ -70,6 +89,12 @@ export default {
         hour: "2-digit",
         minute: "2-digit",
       });
+    },
+    entityList() {
+      if (this.message.entities) {
+        return this.message.entities;
+      }
+      return [];
     },
   },
 };
