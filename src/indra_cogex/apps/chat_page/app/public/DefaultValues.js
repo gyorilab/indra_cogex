6022bb8b64e057862d25840af81a9eb3f56e6033
kkaris <karis.klas@gmail.com>
2022-05-05 14:16:02 -0400

    Separate hp and efo

--- src/indra_cogex/apps/chat_page/app/public/DefaultValues.js
+++ src/indra_cogex/apps/chat_page/app/public/DefaultValues.js
@@ -2,6 +2,7 @@ const badgeMappings = {
   primary: ["fplx", "hgnc", "up", "uppro", "mirbase"], // genes, proteins
   secondary: ["chebi"], // Small molecule
   success: ["go", "mesh", "doid"], // Biological process, disease
-  info: ["hp", "efo"], // Not in any of the above
+  info: ["hp"], // Phenotypic Abnormality
+  light: ["efo"], // Experimental Factor Ontology
 };
 export default badgeMappings;
