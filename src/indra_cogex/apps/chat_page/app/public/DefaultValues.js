feb518ea1e8e105bc223aee5238693141a472e81
kkaris <karis.klas@gmail.com>
2022-05-05 14:07:57 -0400

    Set badge color based on entity type

---
+++ src/indra_cogex/apps/chat_page/app/public/DefaultValues.js
@@ -0,0 +1,7 @@
+const badgeMappings = {
+  primary: ["fplx", "hgnc", "up", "uppro", "mirbase"], // genes, proteins
+  secondary: ["chebi"], // Small molecule
+  success: ["go", "mesh", "doid"], // Biological process, disease
+  info: ["hp", "efo"], // Not in any of the above
+};
+export default badgeMappings;
