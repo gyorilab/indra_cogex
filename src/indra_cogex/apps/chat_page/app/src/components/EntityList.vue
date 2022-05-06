75c8e60bcb938c2575955777565adf6d7d7dd699
kkaris <karis.klas@gmail.com>
2022-05-06 15:49:34 -0400

    Hack together visibility toggle

--- src/indra_cogex/apps/chat_page/app/src/components/EntityList.vue
+++ src/indra_cogex/apps/chat_page/app/src/components/EntityList.vue
@@ -3,12 +3,13 @@
   <span
     v-for="([cls, descr], index) in badgeClasses"
     class="badge"
-    :class="cls"
+    @click="toggleHide(cls)"
+    :class="`${cls} ${isClsVisible(cls) ? '' : ' opacity-50'}`"
     :key="index"
     >{{ descr }}</span
   >
   <hr />
-  <template v-for="(entity, number) in entities" :key="number">
+  <template v-for="(entity, number) in computedList" :key="number">
     <EntityModal :gnd="entity.gnd" :nm="entity.nm" />
   </template>
 </template>
@@ -31,6 +32,12 @@ export default {
         ["bg-light text-dark", "Experimental Factor"],
         ["bg-warning text-dark", "ungrounded"],
       ],
+      primaryVis: true,
+      secondaryVis: true,
+      successVis: true,
+      infoVis: true,
+      lightVis: true,
+      warningVis: true,
     };
   },
   props: {
@@ -40,5 +47,88 @@ export default {
       required: true,
     },
   },
+  computed: {
+    computedList() {
+      let list = [];
+      for (let entity of this.entities) {
+        if (this.isClsVisible(this.nsToCls(entity.gnd))) {
+          list.push(entity);
+        }
+      }
+      return list;
+    },
+  },
+  methods: {
+    toggleHide(cls) {
+      switch (cls) {
+        case "bg-primary":
+          this.primaryVis = !this.primaryVis;
+          break;
+        case "bg-secondary":
+          this.secondaryVis = !this.secondaryVis;
+          break;
+        case "bg-success":
+          this.successVis = !this.successVis;
+          break;
+        case "bg-info text-dark":
+          this.infoVis = !this.infoVis;
+          break;
+        case "bg-light text-dark":
+          this.lightVis = !this.lightVis;
+          break;
+        case "bg-warning text-dark":
+          this.warningVis = !this.warningVis;
+          break;
+      }
+    },
+    isClsVisible(cls) {
+      switch (cls) {
+        case "bg-primary":
+          return this.primaryVis;
+        case "bg-secondary":
+          return this.secondaryVis;
+        case "bg-success":
+          return this.successVis;
+        case "bg-info text-dark":
+          return this.infoVis;
+        case "bg-light text-dark":
+          return this.lightVis;
+        case "bg-warning text-dark":
+          return this.warningVis;
+      }
+    },
+    nsToCls(gnd) {
+      let ns = "";
+      if (gnd[0]) {
+        ns = gnd[0].toLowerCase();
+      }
+      switch (ns) {
+        case "fplx":
+        case "hgnc":
+        case "up":
+        case "uppro":
+        case "mirbase":
+          return "bg-primary";
+        case "chebi":
+          return "bg-secondary";
+        case "go":
+        case "mesh":
+        case "doid":
+          return "bg-success";
+        case "hp":
+          return "bg-info text-dark";
+        case "efo":
+          return "bg-light text-dark";
+        default:
+          return "warning text-dark";
+      }
+    },
+  },
 };
 </script>
+
+<style scoped>
+.badge {
+  cursor: pointer;
+}
+</style>
