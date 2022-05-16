cd80d76fbb9d7c689a7ebbce755acfda74b8eb15
kkaris <karis.klas@gmail.com>
2022-05-16 14:58:07 -0400

    Handle more content types

--- src/indra_cogex/apps/chat_page/app/src/components/MessageWrapper.vue
+++ src/indra_cogex/apps/chat_page/app/src/components/MessageWrapper.vue
@@ -111,11 +111,18 @@
                         v-for="(txtObj, index) in textObjectArray"
                         :key="`${idRegistry.navTabs.content.text}-text${index}`"
                       >
+                        <!-- No object: just display the text as innerHtml -->
                         <span
                           v-if="txtObj.object === null"
                           v-html="txtObj.text"
                         ></span>
-                        <template v-else-if="txtObj.object.type === 'agent'">
+                        <!-- A single agent from query: display as EntityModal -->
+                        <template
+                          v-else-if="
+                            txtObj.object.role === 'fixed' &&
+                            txtObj.object.type === 'agent'
+                          "
+                        >
                           <EntityModal :agent-object="txtObj.object" />
                         </template>
                         <ul v-else-if="txtObj.object.type === 'string_list'">
@@ -126,6 +133,29 @@
                             {{ str }}
                           </li>
                         </ul>
+                        <!-- agent list with reply entities: display the computed string -->
+                        <span
+                          v-else-if="
+                            txtObj.object.type === 'agent_list' &&
+                            txtObj.object.role === 'variable'
+                          "
+                          v-html="txtObj.text"
+                        ></span>
+                        <!-- A single entity from query: display as EntityModal -->
+                        <span v-else-if="txtObj.object.type === 'url_list'">
+                          <span
+                            v-for="([url, db_name], index) in txtObj.object
+                              .value"
+                            :key="`${idRegistry.navTabs.content.text}-urllist${index}`"
+                          >
+                            <a :href="url" target="_blank">{{ db_name }}</a>
+                            {{
+                              index === txtObj.object.value.length - 2
+                                ? "and "
+                                : ", "
+                            }}
+                          </span>
+                        </span>
                       </template>
                     </p>
                   </template>
@@ -253,12 +283,28 @@ export default {
               const obj = this.bot.objects[objName] || null;
               let textForObj;
               if (obj) {
-                textForObj = this.getTextForObj(obj);
-                txtObjs.push({ text: textForObj, object: obj });
+                let formatSpec;
+                if (limit !== null) {
+                  // Get the numeric value of the limit, e.g. "limit10" -> 10
+                  limit = parseInt(limit.replace(/limit/g, ""), 10);
+                  formatSpec = { limit: limit };
+                }
+                textForObj = this.getTextForObj(obj, formatSpec);
+                txtObjs.push({
+                  text: textForObj,
+                  object: obj,
+                  objName: objName,
+                  match: substring, // The string that matched the object
+                });
               }
             } else {
               // If we don't match a recognised object, just add the string
-              txtObjs.push({ text: substring, object: null });
+              txtObjs.push({
+                text: substring,
+                object: null,
+                objName: null,
+                match: substring,
+              });
             }
           }
         });
@@ -325,12 +371,27 @@ export default {
     getTextForObj(obj, format_spec = null) {
       // Return the text for the object
       switch (obj.type) {
-        // Todo: handle 'agent_list', 'stmt_list', 'url', 'url_list', 'string_list', 'str'
+        // Todo: handle 'stmt_list', 'url', 'url_list', 'string_list', 'str'
         case "agent":
           // Return object.value.name
           return obj.value.name;
+        case "agent_list": {
+          // If there is a limit, return the first N agent names as a string
+          let limit = format_spec.limit || obj.value.length;
+          let nameArray = obj.value.slice(0, limit).map((agent) => agent.name);
+          return this.englishJoin(nameArray);
+        }
+        case "string_list":
+        case "url_list":
+          /* string_list, url_list is an arrays of strings that are joined together with
+           * a delimiter in the template and doesn't need any text */
+          return null;
+        case "string":
+          // Return the string
+          return obj.value;
         default:
-          console.log("In switch-case: unhandled object: " + obj.type);
+          console.log("In switch-case; unhandled object type: " + obj.type);
+          console.log(obj);
           return "(N/A)";
       }
     },
