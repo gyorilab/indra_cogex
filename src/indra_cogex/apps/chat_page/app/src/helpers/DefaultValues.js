15dc703276d16bad71bc439d62f5f6f43c2817b3
kkaris <karis.klas@gmail.com>
2022-05-19 17:44:21 -0400

    Add some more namespaces to small molecules

--- src/indra_cogex/apps/chat_page/app/src/helpers/DefaultValues.js
+++ src/indra_cogex/apps/chat_page/app/src/helpers/DefaultValues.js
@@ -1,6 +1,6 @@
 const badgeMappings = {
   ["bg-primary"]: ["fplx", "hgnc", "up", "uppro", "mirbase"], // genes, proteins
-  ["bg-secondary"]: ["chebi"], // Small molecule
+  ["bg-secondary"]: ["chebi", "pubchem", "chembl", "hms-lincs"], // Small molecule
   ["bg-success"]: ["go", "mesh", "doid"], // Biological process, disease
   ["bg-info text-dark"]: ["hp"], // Phenotypic Abnormality
   ["bg-light text-dark"]: ["efo"], // Experimental Factor Ontology
