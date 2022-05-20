6bbc419b9a30d5d156f6879cee0aeca5bdf15484
kkaris <karis.klas@gmail.com>
2022-05-20 13:49:13 -0400

    Install and import bootstrap-icons

--- src/indra_cogex/apps/chat_page/app/src/main.js
+++ src/indra_cogex/apps/chat_page/app/src/main.js
@@ -1,5 +1,6 @@
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
