a9ae3877d756877c2338f4fd4eb5a2de37937b69
kkaris <karis.klas@gmail.com>
2022-05-20 16:16:17 -0400

    Setup English fetching

--- src/indra_cogex/apps/chat_page/app/src/components/StmtList.vue
+++ src/indra_cogex/apps/chat_page/app/src/components/StmtList.vue
@@ -1,19 +1,20 @@
 <template>
   <div
     class="row"
-    v-for="(stmt, index) in stmts"
+    v-for="(stmt, index) in stmtsWithEnglish"
     :key="`${this.componentUUID}-${index}`"
   >
-    <div class="col">
+    <div class="col-auto">
+      <span>{{ stmt.english ? stmt.english : englishFromStmt(stmt) }}</span>
       Agents:
       <span
-        v-for="(ag, innerIndex) in this.getAgentList(stmt)"
+        v-for="(ag, innerIndex) in getAgentList(stmt)"
         :key="`${this.componentUUID}-${index}-${innerIndex}`"
         ><AgentModal :agent-object="ag"
       /></span>
     </div>
     <div class="col text-end">
-      See full statement info:
+      Full statement info:
       <a
         :href="`https://discovery.indra.bio/statement_display?stmt_hash=${stmt.matches_hash}`"
         target="_blank"
@@ -33,6 +34,11 @@ export default {
   components: {
     AgentModal,
   },
+  data() {
+    return {
+      englishLookup: {},
+    };
+  },
   props: {
     stmts: {
       /* Array of statements:
@@ -47,8 +53,8 @@ export default {
        *      - text: String of evidence text
        *      - text_refs: Object containing identifiers for different article sources, e.g. { "PMID": "12345", "doi": "10.1038/ncomms12456" }
        *    }
-       *  - id: Hex String (what is this?)
-       *  - matches_hash: String
+       *  - id: Hex String UUID (what is this?)
+       *  - matches_hash: String of statement hash
        *  - obj: (optional) Object containing the statement object as an Agent: { name: "", db_refs: {...} }
        *  - obj_activity: (optional) String describing the activity of the object
        *  - subj: (optional) Object containing the statement subject as an Agent: { name: "", db_refs: {...} }
@@ -63,6 +69,27 @@ export default {
     componentUUID() {
       return `stmtList-${this.uuid}`;
     },
+    stmtsWithEnglish() {
+      // Extend each statement object with its English assembled name
+      if (Object.values(this.englishLookup).length === 0) {
+        return this.stmts;
+      }
+      let extendedStmts = [];
+      // Look up each statement in the English lookup
+      this.stmts.forEach((stmt) => {
+        const english = this.englishLookup[stmt.id];
+        if (english) {
+          stmt.english = english;
+          extendedStmts.push(stmt);
+        } else {
+          extendedStmts.push(this.englishFromStmt(stmt));
+        }
+      });
+      return extendedStmts;
+    },
+  },
+  mounted() {
+    this.getEnglishArray();
   },
   setup() {
     const uuid = helperFunctions.getUUID()();
@@ -73,13 +100,70 @@ export default {
   methods: {
     getAgentList(stmt) {
       const agentList = [];
-      // Loop through key-value pairs of the statement object and add to agentList the objects that have the keys "name" and "db_refs"
+      // Loop through key-value pairs of the statement object and add
+      // to agentList the objects that have the keys "name" and "db_refs"
       for (const obj of Object.values(stmt)) {
         if (obj.name && obj.db_refs) {
           agentList.push(obj);
         }
       }
       return agentList;
+    },
+    stmtVerb(stmt) {
+      // Return the verb of the statement noun, i.e. "activates" for "Activation"
+      switch (stmt.type.toLowerCase()) {
+        case "complex":
+          return "binds";
+        default:
+          if (stmt.type.endsWith("ition")) {
+            // If suffix is 'ition', replace with 'its'. E.g. 'Inhibition' -> 'Inhibits'
+            return stmt.type.replace(/ition$/, "its");
+          } else if (stmt.type.endsWith("ation")) {
+            // If suffix is ation, replace with ates. E.g. "Activation" -> "Activates"
+            return stmt.type.replace(/ation$/, "ates");
+          } else {
+            // If no suffix, return the type as-is
+            return stmt.type.toLowerCase();
+          }
+      }
+    },
+    englishFromStmt(stmt) {
+      // Backup in case there is no sentence in the English lookup
+      const verb = this.stmtVerb(stmt);
+      if (stmt.type.toLowerCase() === "complex") {
+        // <first agent name> binds ', '.join(rest of agent names)
+        const agentList = this.getAgentList(stmt);
+        const firstAgent = agentList[0];
+        const restOfAgents = agentList.slice(1);
+        const restOfAgentsString = restOfAgents
+          .map((agent) => agent.name)
+          .join(", ");
+        return `${firstAgent.name} ${verb} ${restOfAgentsString}`;
+      }
+      // <first agent name> <type> ', '.join(rest of agent names)
+      const agentList = this.getAgentList(stmt);
+      const firstAgent = agentList[0];
+      const restOfAgents = agentList.slice(1);
+      const restOfAgentsString = restOfAgents
+        .map((agent) => agent.name)
+        .join(", ");
+      return `${firstAgent.name} ${verb} ${restOfAgentsString}`;
+    },
+    async getEnglishArray() {
+      const indraApi = "http://api.indra.bio:8000/assemblers/english";
+      // POST to get the English assembly of the statements
+      const response = await fetch(indraApi, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          statements: this.stmts,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      this.englishLookup = data.sentences;
     },
   },
 };
