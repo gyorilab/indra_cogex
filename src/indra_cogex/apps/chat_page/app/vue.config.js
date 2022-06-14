d3961b5db0edd403b350d6df7d8adf7ddde60db3
kkaris <karis.klas@gmail.com>
2022-06-14 15:38:53 -0400

    Update publicPath to match S3 folder name

--- src/indra_cogex/apps/chat_page/app/vue.config.js
+++ src/indra_cogex/apps/chat_page/app/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/chat_app/",
+  publicPath: "/discovery_chat_app/",
 });
