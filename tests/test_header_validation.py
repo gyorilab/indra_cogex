47400116d87f8d283bc2bfe97cbfe525aaa38ede
kkaris <karis.klas@gmail.com>
2022-08-19 10:32:26 -0700

    Extend header validity tests even more

--- tests/test_header_validation.py
+++ tests/test_header_validation.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from tempfile import TemporaryDirectory
+from typing import Any
+
 from indra_cogex.sources import Processor
 from indra_cogex.representation import Node, Relation
 from indra_cogex.sources.processor import validate_headers
@@ -33,20 +35,27 @@ class MockProcessor(Processor, object):
     name = "mock_processor"
     node_types = ["mock_node"]
     data_type: str = NotImplemented
+    col_name: str = NotImplemented
+    num_nodes: int = 3
+
+    @staticmethod
+    def data_value(n: int) -> Any:
+        """Return a data value for the given node."""
+        raise NotImplementedError
 
     def get_nodes(self):
-        """Return an empty sequence of nodes for testing."""
-        for n in range(3):
+        """Return a sequence of nodes for testing."""
+        for n in range(self.num_nodes):
             yield Node(
                 db_ns="PUBMED",
                 db_id=str(n),
                 labels=["MockNode"],
-                data={"an_int:int": n, f"raises_exception:{self.data_type}": n ** 2},
+                data={f"{self.col_name}:{self.data_type}": self.data_value(n)},
             )
 
     def get_relations(self):
-        """Return an empty sequence of relations for testing."""
-        num = 3
+        """Return a sequence of relations for testing."""
+        num = self.num_nodes
         for n in range(num):
             yield Relation(
                 source_ns="PUBMED",
@@ -58,15 +67,45 @@ class MockProcessor(Processor, object):
             )
 
 
-class BadProcessor(MockProcessor):
+class BadTypeProcessor(MockProcessor):
     data_type = "bad_type"
+    col_name = "colname"
+
+    @staticmethod
+    def data_value(n: int) -> int:
+        return n ** 2
 
 
-class GoodProcessor(MockProcessor):
+class GoodTypeProcessor(MockProcessor):
     data_type = "int"
+    col_name = "colname"
+
+    @staticmethod
+    def data_value(n: int) -> int:
+        return n ** 2
 
 
-def test_data_type_validator():
+class BadArrayTypeProcessor(MockProcessor):
+    data_type = "bad_type[]"
+    col_name = "array_colname"
+
+    @staticmethod
+    def data_value(n: int) -> str:
+        m = int((n + 1) ** 2)
+        return f"[{';'.join(str(k) for k in range(m))}]"
+
+
+class GoodArrayTypeProcessor(MockProcessor):
+    data_type = "int[]"
+    col_name = "array_colname"
+
+    @staticmethod
+    def data_value(n: int) -> str:
+        m = int((n + 1) ** 2)
+        return f"[{';'.join(str(k) for k in range(m))}]"
+
+
+def test_data_type_validator_bad():
     with TemporaryDirectory() as temp_dir:
         directory = Path(temp_dir)
 
@@ -79,7 +118,7 @@ def test_data_type_validator():
         MockProcessor.nodes_indra_path = directory / "nodes.pkl"
         MockProcessor.edges_path = directory / "edges.tsv.gz"
 
-        mp = BadProcessor()
+        mp = BadTypeProcessor()
         try:
             mp.dump()
         except Exception as e:
@@ -97,15 +136,56 @@ def test_data_type_validator_good():
         processor_dir.mkdir()
 
         # override the __init_subclass__ with the directory for this test
-        GoodProcessor.directory = directory
-        GoodProcessor.nodes_path = directory / "nodes.tsv.gz"
-        GoodProcessor.nodes_indra_path = directory / "nodes.pkl"
-        GoodProcessor.edges_path = directory / "edges.tsv.gz"
+        GoodTypeProcessor.directory = directory
+        GoodTypeProcessor.nodes_path = directory / "nodes.tsv.gz"
+        GoodTypeProcessor.nodes_indra_path = directory / "nodes.pkl"
+        GoodTypeProcessor.edges_path = directory / "edges.tsv.gz"
 
-        mp = GoodProcessor()
+        mp = GoodTypeProcessor()
         try:
             mp.dump()
         except Exception as e:
             assert False, f"Unexpected exception: {repr(e)}"
+
+
+def test_array_data_type_validator_bad():
+    with TemporaryDirectory() as temp_dir:
+        directory = Path(temp_dir)
+
+        processor_dir = directory / "output"
+        processor_dir.mkdir()
+
+        # override the __init_subclass__ with the directory for this test
+        BadArrayTypeProcessor.directory = directory
+        BadArrayTypeProcessor.nodes_path = directory / "nodes.tsv.gz"
+        BadArrayTypeProcessor.nodes_indra_path = directory / "nodes.pkl"
+        BadArrayTypeProcessor.edges_path = directory / "edges.tsv.gz"
+
+        mp = BadArrayTypeProcessor()
+        try:
+            mp.dump()
+        except Exception as e:
+            assert isinstance(e, TypeError)
+            assert "bad_type[]" in str(e)
         else:
-            assert True
+            assert False, "Expected exception"
+
+
+def test_array_data_type_validator_good():
+    with TemporaryDirectory() as temp_dir:
+        directory = Path(temp_dir)
+
+        processor_dir = directory / "output"
+        processor_dir.mkdir()
+
+        # override the __init_subclass__ with the directory for this test
+        GoodArrayTypeProcessor.directory = directory
+        GoodArrayTypeProcessor.nodes_path = directory / "nodes.tsv.gz"
+        GoodArrayTypeProcessor.nodes_indra_path = directory / "nodes.pkl"
+        GoodArrayTypeProcessor.edges_path = directory / "edges.tsv.gz"
+
+        mp = GoodArrayTypeProcessor()
+        try:
+            mp.dump()
+        except Exception as e:
+            assert False, f"Unexpected exception: {repr(e)}"
