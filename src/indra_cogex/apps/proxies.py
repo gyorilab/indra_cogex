ddc48465d8c8b02843d9e35959d81dfd008a13ae
kkaris <karis.klas@gmail.com>
2022-08-26 11:53:48 -0700

    Move curation cache to its own sub module

--- src/indra_cogex/apps/proxies.py
+++ src/indra_cogex/apps/proxies.py
@@ -9,7 +9,7 @@ from flask import current_app, request
 from werkzeug.local import LocalProxy
 
 from indra_cogex.apps.constants import INDRA_COGEX_EXTENSION, STATEMENT_CURATION_CACHE
-from indra_cogex.apps.curator import CurationCache
+from indra_cogex.apps.curation_cache import CurationCache
 from indra_cogex.client.neo4j_client import Neo4jClient
 
 __all__ = [
