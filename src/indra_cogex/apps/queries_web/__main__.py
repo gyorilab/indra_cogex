026c85892222b8b2932b80b394c2dde03020b940
kkaris <karis.klas@gmail.com>
2022-09-22 14:55:11 -0700

    Fix cli import

--- src/indra_cogex/apps/queries_web/__main__.py
+++ src/indra_cogex/apps/queries_web/__main__.py
@@ -2,7 +2,7 @@
 
 """Run the query web app with ``python -m indra_cogex.apps.queries_web``."""
 
-from . import cli
+from indra_cogex.apps.queries_web.cli import cli
 
 if __name__ == "__main__":
     cli()
