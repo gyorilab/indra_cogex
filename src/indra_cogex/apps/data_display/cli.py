e818d87663173dcff3cfbe7f696b98d75d8a3c5a
kkaris <karis.klas@gmail.com>
2024-01-24 15:09:29 -0800

    Update so data display can be run on its own

--- src/indra_cogex/apps/data_display/cli.py
+++ src/indra_cogex/apps/data_display/cli.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap4
+from indra_cogex.apps.curation_cache import CurationCache
 from indralab_auth_tools.auth import auth, config_auth
 from more_click import make_web_command
 
-from indra_cogex.apps.constants import INDRA_COGEX_EXTENSION, STATIC_DIR, TEMPLATES_DIR
+from indra_cogex.apps.constants import (
+    INDRA_COGEX_EXTENSION,
+    STATIC_DIR,
+    TEMPLATES_DIR,
+    STATEMENT_CURATION_CACHE
+)
 from indra_cogex.apps.data_display import data_display_blueprint
 from indra_cogex.client.neo4j_client import Neo4jClient
 
 app = Flask(__name__, template_folder=TEMPLATES_DIR, static_folder=STATIC_DIR)
 bootstrap = Bootstrap4(app)
 app.extensions[INDRA_COGEX_EXTENSION] = Neo4jClient()
+app.extensions[STATEMENT_CURATION_CACHE] = CurationCache()
 app.register_blueprint(auth)
 app.register_blueprint(data_display_blueprint)
 SC, jwt = config_auth(app)
