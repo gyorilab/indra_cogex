83839474e8d313e89cf5ca8233de9d7d31258121
kkaris <karis.klas@gmail.com>
2024-05-13 16:41:28 -0700

    Add test for iter_resolvers

---
+++ tests/test_sources/test_source_import.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import importlib
+
+from indra_cogex.sources.cli import _iter_resolvers
+
+
+def get_processor_classes():
+    # Assumes that all processors are in their own submodules of indra_cogex.sources
+    # either in directories or in python files
+    from indra_cogex.sources import Processor
+    from indra_cogex import sources
+
+    sources_path = Path(sources.__file__).parent
+    yielded = set()
+    for submodule_path in sources_path.iterdir():
+        if submodule_path.name.startswith("_"):
+            continue
+        submodule_name = submodule_path.name
+        if submodule_name.endswith(".py"):
+            submodule_name = submodule_name[:-3]
+        submodule_path = f"indra_cogex.sources.{submodule_name}"
+        submodule = importlib.import_module(submodule_path)
+        for name in dir(submodule):
+            if name.startswith("_"):
+                continue
+            obj = getattr(submodule, name)
+            if isinstance(obj, type) and issubclass(obj, Processor):
+                yielded.add(obj)
+                yield obj
+
+
+def test_source_import():
+    all_subclasses = {cls.__name__ for cls in get_processor_classes()}
+    iter_resolve_classes = {cls.__name__ for cls in _iter_resolvers()}
+    ignore = {"Processor", "PyoboProcessor", "WikiDataProcessor"}
+    actual = iter_resolve_classes - ignore
+    expected = all_subclasses - ignore
+    assert expected == actual, (
+        f"Expected {expected} but got {actual}.\nMissing: "
+        f"{expected - actual},\nExtra: {actual - expected}"
+    )
