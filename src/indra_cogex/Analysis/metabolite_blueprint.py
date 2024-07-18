3a4a1f1c318c56cb579d23b07c723616b5e1d388
Prasham Marfatia <marfatia.p@northeastern.edu>
2024-07-18 10:31:23 -0400

    Refactor:Removed the parse_metabolites_field function

--- src/indra_cogex/Analysis/metabolite_blueprint.py
+++ src/indra_cogex/Analysis/metabolite_blueprint.py
@@ -1,4 +1,4 @@
-"""Metabolite-centric analysis blueprint."""
+"""Metabolite-centric analysis."""
 
 from typing import Dict, List, Mapping, Tuple
 import pandas as pd
@@ -10,50 +10,10 @@ from indra_cogex.client.enrichment.mla import (
 )
 
 
-
-
-def parse_metabolites_field(s: str) -> Tuple[Dict[str, str], List[str]]:
-    """Parse a string of metabolite identifiers into ChEBI IDs and names.
-
-    Args:
-    s (str): A string containing metabolite identifiers (ChEBI IDs or CURIEs)
-
-    Returns:
-    Tuple[Dict[str, str], List[str]]: A tuple containing a dictionary of ChEBI IDs to metabolite names,
-    and a list of any metabolite identifiers that couldn't be parsed."""
-    records = {
-        record.strip().strip('"').strip("'").strip()
-        for line in s.strip().lstrip("[").rstrip("]").split()
-        if line
-        for record in line.strip().split(",")
-        if record.strip()
-    }
-    chebi_ids = []
-    errors = []
-    for entry in records:
-        if entry.isnumeric():
-            chebi_ids.append(entry)
-        elif entry.lower().startswith("chebi:chebi:"):
-            chebi_ids.append(entry.lower().replace("chebi:chebi:", "", 1))
-        elif entry.lower().startswith("chebi:"):
-            chebi_ids.append(entry.lower().replace("chebi:", "", 1))
-        else:  # probably a name, do our best
-            chebi_id = chebi_client.get_chebi_id_from_name(entry)
-            if chebi_id:
-                chebi_ids.append(chebi_id)
-            else:
-                errors.append(entry)
-    metabolites = {
-        chebi_id: chebi_client.get_chebi_name_from_id(chebi_id)
-        for chebi_id in chebi_ids
-    }
-    return metabolites, errors
-
 def discrete_analysis(client, metabolites: str, method: str, alpha: float, keep_insignificant: bool,
                           minimum_evidence_count: int, minimum_belief: float):
     """Perform discrete metabolite set analysis using metabolomics over-representation analysis.
 
-    Args:
     client: The client object for making API calls
     metabolites (str): A string of metabolite identifiers
     method (str): The statistical method for multiple testing correction
@@ -86,7 +46,7 @@ def discrete_analysis(client, metabolites: str, method: str, alpha: float, keep_
 def enzyme_analysis(client, ec_code: str, chebi_ids: List[str] = None):
     """Perform enzyme analysis and explanation for given EC code and optional ChEBI IDs.
 
-    Args:
+
     client: The client object for making API calls
     ec_code (str): The EC code for the enzyme
     chebi_ids (List[str], optional): List of ChEBI IDs for additional context
@@ -97,3 +57,6 @@ def enzyme_analysis(client, ec_code: str, chebi_ids: List[str] = None):
         client=client, ec_code=ec_code, chebi_ids=chebi_ids
     )
     return stmts
+
+
+
