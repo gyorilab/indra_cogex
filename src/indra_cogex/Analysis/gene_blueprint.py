885ee628ba55429686f27a769de2dce81fd33e0b
Prasham Marfatia <marfatia.p@northeastern.edu>
2024-07-18 08:32:53 -0400

    Refactor: Updating the discre_analysis function to recieve parsed_list input

--- src/indra_cogex/Analysis/gene_blueprint.py
+++ src/indra_cogex/Analysis/gene_blueprint.py
@@ -27,44 +27,63 @@ from indra_cogex.client.enrichment.discrete import (
 from ...client.enrichment.signed import reverse_casual_reasoning
 
 
-def discrete_analysis(client, genes: str, method: str, alpha: float, keep_insignificant: bool,
-                      minimum_evidence_count: int, minimum_belief: float):
-    """Perform discrete gene set analysis using various enrichment methods.
+def discrete_analysis(client, genes: Dict[str, str], method: str, alpha: float,
+                      keep_insignificant: bool, minimum_evidence_count: int,
+                      minimum_belief: float) -> Dict:
+    """
+    Perform discrete gene set analysis using various enrichment methods.
 
-    Args:
-    client: The client object for making API calls
-    genes (str): A string of gene identifiers
-    method (str): The statistical method for multiple testing correction
-    alpha (float): The significance level
-    keep_insignificant (bool): Whether to keep statistically insignificant results
-    minimum_evidence_count (int): Minimum number of evidence required for INDRA analysis
-    minimum_belief (float): Minimum belief score for INDRA analysis
+    Parameters
+    ----------
+    client : object
+        The client object for making API calls.
+    genes : dict
+        A dictionary of HGNC IDs to gene names.
+    method : str
+        The statistical method for multiple testing correction.
+    alpha : float
+        The significance level.
+    keep_insignificant : bool
+        Whether to keep statistically insignificant results.
+    minimum_evidence_count : int
+        Minimum number of evidence required for INDRA analysis.
+    minimum_belief : float
+        Minimum belief score for INDRA analysis.
 
-    Returns:
-    dict: A dictionary containing results from various analyses"""
-    genes, errors = parse_genes_field(genes)
-    gene_set = set(genes)
+    Returns
+    -------
+    dict
+        A dictionary containing results from various analyses.
+    """
+    gene_set = set(genes.keys())
 
     go_results = go_ora(
-        client, gene_set, method=method, alpha=alpha, keep_insignificant=keep_insignificant
+        client, gene_set, method=method, alpha=alpha,
+        keep_insignificant=keep_insignificant
     )
     wikipathways_results = wikipathways_ora(
-        client, gene_set, method=method, alpha=alpha, keep_insignificant=keep_insignificant
+        client, gene_set, method=method, alpha=alpha,
+        keep_insignificant=keep_insignificant
     )
     reactome_results = reactome_ora(
-        client, gene_set, method=method, alpha=alpha, keep_insignificant=keep_insignificant
+        client, gene_set, method=method, alpha=alpha,
+        keep_insignificant=keep_insignificant
     )
     phenotype_results = phenotype_ora(
-        gene_set, client=client, method=method, alpha=alpha, keep_insignificant=keep_insignificant
+        gene_set, client=client, method=method, alpha=alpha,
+        keep_insignificant=keep_insignificant
     )
-
     indra_upstream_results = indra_upstream_ora(
-        client, gene_set, method=method, alpha=alpha, keep_insignificant=keep_insignificant,
-        minimum_evidence_count=minimum_evidence_count, minimum_belief=minimum_belief
+        client, gene_set, method=method, alpha=alpha,
+        keep_insignificant=keep_insignificant,
+        minimum_evidence_count=minimum_evidence_count,
+        minimum_belief=minimum_belief
     )
     indra_downstream_results = indra_downstream_ora(
-        client, gene_set, method=method, alpha=alpha, keep_insignificant=keep_insignificant,
-        minimum_evidence_count=minimum_evidence_count, minimum_belief=minimum_belief
+        client, gene_set, method=method, alpha=alpha,
+        keep_insignificant=keep_insignificant,
+        minimum_evidence_count=minimum_evidence_count,
+        minimum_belief=minimum_belief
     )
 
     return {
@@ -73,45 +92,18 @@ def discrete_analysis(client, genes: str, method: str, alpha: float, keep_insign
         "reactome_results": reactome_results,
         "phenotype_results": phenotype_results,
         "indra_upstream_results": indra_upstream_results,
-        "indra_downstream_results": indra_downstream_results,
-        "errors": errors
+        "indra_downstream_results": indra_downstream_results
     }
 
-    def signed_analysis(client, positive_genes: str, negative_genes: str, alpha: float,
-                        keep_insignificant: bool, minimum_evidence_count: int, minimum_belief: float):
-         """Perform signed gene set analysis using reverse causal reasoning.
 
-    Args:
-    client: The client object for making API calls
-    positive_genes (str): A string of gene identifiers for positively regulated genes
-    negative_genes (str): A string of gene identifiers for negatively regulated genes
-    alpha (float): The significance level
-    keep_insignificant (bool): Whether to keep statistically insignificant results
-    minimum_evidence_count (int): Minimum number of evidence required
-    minimum_belief (float): Minimum belief score required
 
-    Returns:
-    dict: A dictionary containing results and any parsing errors"""
-    positive_genes, positive_errors = parse_genes_field(positive_genes)
-    negative_genes, negative_errors = parse_genes_field(negative_genes)
 
-    results = reverse_causal_reasoning(
-        client=client,
-        positive_hgnc_ids=positive_genes,
-        negative_hgnc_ids=negative_genes,
-        alpha=alpha,
-        keep_insignificant=keep_insignificant,
-        minimum_evidence_count=minimum_evidence_count,
-        minimum_belief=minimum_belief,
-    )
 
-    return {
-        "results": results,
-        "positive_errors": positive_errors,
-        "negative_errors": negative_errors
-    }
 
-    def continuous_analysis(client, file_path: str, gene_name_column: str, log_fold_change_column: str,
+
+
+
+def continuous_analysis(client, file_path: str, gene_name_column: str, log_fold_change_column: str,
                             species: str, permutations: int, alpha: float, keep_insignificant: bool,
                             source: str, minimum_evidence_count: int, minimum_belief: float):
          """Perform continuous gene set analysis on gene expression data.
