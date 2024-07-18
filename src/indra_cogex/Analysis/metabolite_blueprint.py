f1e4a409b4a2cd08bfa73d0c6120c8355250becd
Prasham Marfatia <marfatia.p@northeastern.edu>
2024-07-18 10:34:13 -0400

    Refactor:Updated the discrete_analysis function to accept pre-parsed metabolites

--- src/indra_cogex/Analysis/metabolite_blueprint.py
+++ src/indra_cogex/Analysis/metabolite_blueprint.py
@@ -10,25 +10,35 @@ from indra_cogex.client.enrichment.mla import (
 )
 
 
-def discrete_analysis(client, metabolites: str, method: str, alpha: float, keep_insignificant: bool,
-                          minimum_evidence_count: int, minimum_belief: float):
+def discrete_analysis(client, metabolites: Dict[str, str], method: str, alpha: float,
+                      keep_insignificant: bool, minimum_evidence_count: int,
+                      minimum_belief: float) -> Dict:
     """Perform discrete metabolite set analysis using metabolomics over-representation analysis.
 
-    client: The client object for making API calls
-    metabolites (str): A string of metabolite identifiers
-    method (str): The statistical method for multiple testing correction
-    alpha (float): The significance level
-    keep_insignificant (bool): Whether to keep statistically insignificant results
-    minimum_evidence_count (int): Minimum number of evidence required for analysis
-    minimum_belief (float): Minimum belief score for analysis
+    Parameters
+    ----------
+    client : object
+        The client object for making API calls.
+    metabolites : dict
+        A dictionary of ChEBI IDs to metabolite names.
+    method : str
+        The statistical method for multiple testing correction.
+    alpha : float
+        The significance level.
+    keep_insignificant : bool
+        Whether to keep statistically insignificant results.
+    minimum_evidence_count : int
+        Minimum number of evidence required for analysis.
+    minimum_belief : float
+        Minimum belief score for analysis.
 
-    Returns:
-    dict: A dictionary containing results from the analysis"""
-    metabolite_chebi_ids, errors = parse_metabolites_field(metabolites)
-
+    Returns
+    -------
+    dict
+        A dictionary containing results from the analysis."""
     results = metabolomics_ora(
         client=client,
-        chebi_ids=metabolite_chebi_ids,
+        chebi_ids=metabolites,
         method=method,
         alpha=alpha,
         keep_insignificant=keep_insignificant,
@@ -37,8 +47,7 @@ def discrete_analysis(client, metabolites: str, method: str, alpha: float, keep_
     )
 
     return {
-        "metabolites": metabolite_chebi_ids,
-        "errors": errors,
+        "metabolites": metabolites,
         "results": results
     }
 
