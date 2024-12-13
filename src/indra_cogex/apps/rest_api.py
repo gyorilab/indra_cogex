cc5ca53873ce558c8329d7fedd7251d64fb65d0b
Prasham Marfatia <marfatia.p@northeastern.edu>
2024-12-13 15:24:44 -0500

    Add imports and namespaces for each function category

--- src/indra_cogex/apps/rest_api.py
+++ src/indra_cogex/apps/rest_api.py
@@ -1,7 +1,8 @@
 from flask_restx import Api
 
 from .bioentity.api import bioentity_ns
-from .queries_web import query_ns
+from .queries_web import search_ns, validation_ns, evidence_ns, relationship_ns, disease_ns, publication_ns, \
+    ontology_ns, cell_line_ns, clinical_ns, mutation_ns, analysis_ns, network_ns
 
 __all__ = [
     "api",
@@ -13,5 +14,16 @@ api = Api(
     doc="/apidocs",
 )
 
-api.add_namespace(query_ns)
+api.add_namespace(search_ns)
+api.add_namespace(validation_ns)
+api.add_namespace(evidence_ns)
+api.add_namespace(relationship_ns)
+api.add_namespace(disease_ns)
+api.add_namespace(publication_ns)
+api.add_namespace(ontology_ns)
+api.add_namespace(cell_line_ns)
+api.add_namespace(clinical_ns)
+api.add_namespace(mutation_ns)
+api.add_namespace(analysis_ns)
+api.add_namespace(network_ns)
 api.add_namespace(bioentity_ns)
