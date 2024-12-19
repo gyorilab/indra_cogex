77be9817959410fcb5e3bad983192853e602356e
Prasham Marfatia <marfatia.p@northeastern.edu>
2024-12-19 15:27:11 -0500

    Add new namespaces according to the updated structure

--- src/indra_cogex/apps/rest_api.py
+++ src/indra_cogex/apps/rest_api.py
@@ -3,10 +3,29 @@ from .bioentity.api import bioentity_ns
 
 # Import and add namespaces after api is created
 from .queries_web import (
-    search_ns, validation_ns, evidence_ns, relationship_ns,
-    disease_ns, publication_ns, ontology_ns, cell_line_ns,
-    clinical_ns, mutation_ns, analysis_ns, network_ns
+    gene_expression_ns,
+    go_terms_ns,
+    clinical_trials_ns,
+    biological_pathways_ns,
+    drug_side_effects_ns,
+    ontology_ns,
+    literature_metadata_ns,
+    statements_ns,
+    drug_targets_ns,
+    cell_markers_ns,
+    disease_phenotypes_ns,
+    gene_disease_variant_ns,
+    research_project_output_ns,
+    gene_domains_ns,
+    phenotype_variant_ns,
+    drug_indications_ns,
+    gene_codependence_ns,
+    enzyme_activity_ns,
+    cell_line_properties_ns,
+    analysis_ns,
+    subnetwork_ns
 )
+
 
 api = Api(
     title="INDRA CoGEx Query API",
@@ -16,18 +35,27 @@ api = Api(
 
 
 # Add all namespaces
-api.add_namespace(search_ns)
-api.add_namespace(validation_ns)
-api.add_namespace(evidence_ns)
-api.add_namespace(relationship_ns)
-api.add_namespace(disease_ns)
-api.add_namespace(publication_ns)
-api.add_namespace(cell_line_ns)
+api.add_namespace(gene_expression_ns)
+api.add_namespace(go_terms_ns)
+api.add_namespace(clinical_trials_ns)
+api.add_namespace(biological_pathways_ns)
+api.add_namespace(drug_side_effects_ns)
 api.add_namespace(ontology_ns)
-api.add_namespace(clinical_ns)
-api.add_namespace(mutation_ns)
+api.add_namespace(literature_metadata_ns)
+api.add_namespace(statements_ns)
+api.add_namespace(drug_targets_ns)
+api.add_namespace(cell_markers_ns)
+api.add_namespace(disease_phenotypes_ns)
+api.add_namespace(gene_disease_variant_ns)
+api.add_namespace(research_project_output_ns)
+api.add_namespace(gene_domains_ns)
+api.add_namespace(phenotype_variant_ns)
+api.add_namespace(drug_indications_ns)
+api.add_namespace(gene_codependence_ns)
+api.add_namespace(enzyme_activity_ns)
+api.add_namespace(cell_line_properties_ns)
 api.add_namespace(analysis_ns)
-api.add_namespace(network_ns)
+api.add_namespace(subnetwork_ns)
 api.add_namespace(bioentity_ns)
 
 __all__ = ["api"]
