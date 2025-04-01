9005ca2ad14cf01584594e52310e88921e1432ad
haohangyan <hy2664@nyu.edu>
2025-04-01 14:53:54 -0400

    Add functionality for cache command

---
+++ src/indra_cogex/apps/search/utils.py
@@ -0,0 +1,19 @@
+import argparse
+import pickle
+
+from indra_cogex.apps.constants import AGENT_NAME_CACHE
+from indra_cogex.client import Neo4jClient
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Utility for search-related tasks.")
+    parser.add_argument(
+        "--cache", action="store_true",
+        help="Load and save agent cache to file."
+    )
+    args = parser.parse_args()
+    if args.cache:
+        neo4j_client = Neo4jClient()
+        agent_cache = neo4j_client.load_agent_cache()
+        with open(AGENT_NAME_CACHE, "wb") as f:
+            pickle.dump(agent_cache, f)
