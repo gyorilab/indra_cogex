faf39f5121a285ae4cc9ca3ef7348d03fca001c5
kkaris <karis.klas@gmail.com>
2025-04-07 11:28:10 -0700

    Explain what running the queries web app module alone does

--- src/indra_cogex/apps/queries_web/__main__.py
+++ src/indra_cogex/apps/queries_web/__main__.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
-"""Run the query web app with ``python -m indra_cogex.apps.queries_web``."""
+"""Run the query web app with ``python -m indra_cogex.apps.queries_web``.
+
+This allows the queries web application to be run as a standalone module, which will
+skip the typical startup activities associated with running the full INDRA-Cogex application.
+In this mode, most pages, including the landing page, will not be available, but it's
+still possible to navigate directly to the api docs at /apidocs and investigate the API.
+"""
 
 from indra_cogex.apps.queries_web.cli import cli
 
