b987e4a02e098577aa7f76e8a612e403f35f86be
haohangyan <hy2664@nyu.edu>
2025-04-08 15:25:28 -0400

    Download latest source_count and processed_stmts

--- src/indra_cogex/sources/indra_db/raw_export.py
+++ src/indra_cogex/sources/indra_db/raw_export.py
@@ -1,362 +1,53 @@
 import logging
-import os
-from collections import defaultdict
 import csv
 import gzip
-import json
+from pathlib import Path
+
 import tqdm
-import codecs
-import pandas
-import pickle
-from adeft.download import get_available_models
-from indra.util import batch_iter
-from indra.statements import stmts_from_json, stmt_from_json
-from indra.tools import assemble_corpus as ac
+
+from indra.statements import stmt_from_json
 from indra_cogex.sources.indra_db.locations import *
 from indra_cogex.util import load_stmt_json_str
 
 logger = logging.getLogger(__name__)
 
 
-def reader_prioritize(reader_contents):
-    drop = set()
-    # We first organize the contents by source/text type
-    versions_per_type = defaultdict(list)
-    for reading_id, reader_version, source, text_type in reader_contents:
-        versions_per_type[(source, text_type)].append((reader_version, reading_id))
-    # Then, within each source/text_type key, we sort according to reader
-    # version to be able to select the newest reader version for each key
-    reading_id_per_type = {}
-    for (source, text_type), versions in versions_per_type.items():
-        if len(versions) > 1:
-            sorted_versions = sorted(
-                versions,
-                key=lambda x: reader_versions[version_to_reader[x[0]]].index(x[0]),
-                reverse=True,
-            )
-            drop |= {x[1] for x in sorted_versions[1:]}
-            reading_id_per_type[(source, text_type)] = sorted_versions[0][1]
-        else:
-            reading_id_per_type[(source, text_type)] = versions[0][1]
-    fulltexts = [
-        content
-        for content in reader_contents
-        if content[3] == "fulltext" and content[0] not in drop
-    ]
-    not_fulltexts = [
-        content
-        for content in reader_contents
-        if content[3] != "fulltext" and content[0] not in drop
-    ]
-    # There are 3 types of non-fulltext content: CORD-19 abstract, PubMed abstract
-    # and PubMed title. If we have CORD-19, we prioritize it because it includes
-    # the title so we drop any PubMed readings. Otherwise we don't drop anything
-    # because we want to keep both the title and the abstract (which doesn't include
-    # the title).
-    if not fulltexts:
-        if ("cord19_abstract", "abstract") in reading_id_per_type:
-            if ("pubmed", "abstract") in reading_id_per_type:
-                drop.add(reading_id_per_type[("pubmed", "abstract")])
-            if ("pubmed", "title") in reading_id_per_type:
-                drop.add(reading_id_per_type[("pubmed", "title")])
-    # In case of fulltext, we can drop all non-fulltexts, and then drop
-    # everything that is lower on the fulltext priority order
-    else:
-        priority = [
-            "xdd-pubmed",
-            "xdd",
-            "xdd-biorxiv",
-            "cord19_pdf",
-            "elsevier",
-            "cord19_pmc_xml",
-            "manuscripts",
-            "pmc_oa",
-        ]
-        drop |= {c[0] for c in not_fulltexts}
-        sorted_fulltexts = sorted(
-            fulltexts, key=lambda x: priority.index(x[2]), reverse=True
-        )
-        drop |= {c[0] for c in sorted_fulltexts[1:]}
-    return drop
+def get_latest_timestamp_prefix(bucket: str, prefix: str) -> str:
+    import boto3
+    from datetime import datetime
+    s3 = boto3.client("s3")
+    paginator = s3.get_paginator("list_objects_v2")
+    result = paginator.paginate(Bucket=bucket, Prefix=prefix, Delimiter="/")
+    timestamps = []
+    for page in result:
+        for cp in page.get("CommonPrefixes", []):
+            folder_name = cp["Prefix"].split("/")[-2]
+            try:
+                timestamps.append(datetime.strptime(folder_name, "%Y%m%d-%H%M%S"))
+            except ValueError:
+                continue
+    if not timestamps:
+        raise ValueError("No valid timestamped folders found in S3.")
 
+    latest_ts = max(timestamps).strftime("%Y%m%d-%H%M%S")
+    return f"{prefix}{latest_ts}/"
 
-def load_text_refs_by_trid(fname: str):
-    text_refs = {}
-    for line in tqdm.tqdm(
-        gzip.open(fname, "rt", encoding="utf-8"),
-        total=36223169,
-        unit_scale=True,
-        desc="Processing text refs into a lookup dictionary",
-    ):
-        ids = line.strip().split("\t")
-        id_names = ["TRID", "PMID", "PMCID", "DOI", "PII", "URL", "MANUSCRIPT_ID"]
-        d = {}
-        for id_name, id_val in zip(id_names, ids):
-            if id_val != "\\N":
-                if id_name == "TRID":
-                    id_val = int(id_val)
-                d[id_name] = id_val
-        text_refs[int(ids[0])] = d
-    return text_refs
-
-
-def get_update(start_date):
-    # start date e.g., '20220112 00:00:00'
-    query = """
-        SELECT tc.insert_date, tr.pmid, tr.pmcid
-        FROM public.text_content as tc, public.text_ref as tr
-        WHERE tc.text_ref_id = tr.id
-        AND tc.insert_date >= start_date
-    """
-
+def download_s3_file(bucket: str, s3_key: str, local_path: Path):
+    import boto3
+    s3 = boto3.client("s3")
+    local_path.parent.mkdir(parents=True, exist_ok=True)
+    s3.download_file(Bucket=bucket, Key=s3_key, Filename=str(local_path))
+    logger.info(f"Downloaded s3://{bucket}/{s3_key} → {local_path}")
 
 if __name__ == "__main__":
-    reader_versions = {
-        "sparser": [
-            "sept14-linux\n",
-            "sept14-linux",
-            "June2018-linux",
-            "October2018-linux",
-            "February2020-linux",
-            "April2020-linux",
-        ],
-        "reach": [
-            "61059a-biores-e9ee36",
-            "1.3.3-61059a-biores-",
-            "1.6.1",
-            "1.6.3-e48717",
-        ],
-        "trips": ["STATIC", "2019Nov14", "2021Jan26"],
-        "isi": ["20180503"],
-        "eidos": ["0.2.3-SNAPSHOT", "1.7.1-SNAPSHOT"],
-        "mti": ["1.0"],
-    }
 
-    version_to_reader = {}
-    for reader, versions in reader_versions.items():
-        for version in versions:
-            version_to_reader[version] = reader
-
-    # STAGE 0: Dump tsv.gz files directly from the principal database.
-    # These can be done from the command line, in the folder
-    # that corresponds to pystow.join('indra', 'db')
-
-    command_line_instructions = """
-    NOTE: it is essential that the file dumps are synced, i.e. run 
-    immediately after each other, otherwise there is a risk that Evidence 
-    and Publication nodes in the end are missing from new statements picked 
-    up in the time between dumps. Additionally, to avoid the risk of having 
-    statements with missing reading data, the raw statements dump should be 
-    run *first*, followed by the other two dumps:
-
-    Raw statements
-
-      psql -d indradb_test -h indradb-refresh.cwcetxbvbgrf.us-east-1.rds.amazonaws.com 
-      -U tester -c "COPY (SELECT id, db_info_id, reading_id, convert_from(json::bytea, 'utf-8') FROM public.raw_statements) 
-      TO STDOUT" | gzip > raw_statements.tsv.gz
-
-    Time estimate: ~30-40 mins
-
-    Text content joined with reading
-
-      psql -d indradb_test -h indradb-refresh.cwcetxbvbgrf.us-east-1.rds.amazonaws.com
-      -U tester -c "COPY (SELECT rd.id, rd.reader_version, tc.id, tc.text_ref_id, 
-      tc.source, tc.text_type FROM public.text_content as tc, public.reading as rd
-      WHERE tc.id = rd.text_content_id) TO STDOUT" | gzip > reading_text_content_meta.tsv.gz
-
-    Time estimate: ~15 mins
-
-    Text refs
-
-      psql -d indradb_test -h indradb-refresh.cwcetxbvbgrf.us-east-1.rds.amazonaws.com
-      -U tester -c "COPY (SELECT id, pmid, pmcid, doi, pii, url, manuscript_id
-      FROM public.text_ref) TO STDOUT" | gzip > text_refs_principal.tsv.gz
-
-    Time estimate: ~2.5 mins
-    """
-
-    # This checks for the initial files dumped directly from the principal
-    # database and raises an error if they are not found
-    needed_files = [reading_text_content_fname, text_refs_fname, raw_stmts_fname]
-    if any(not f.exists() for f in needed_files):
-        missing = [f.as_posix() for f in needed_files if not f.exists()]
-        print(command_line_instructions)
-        raise FileNotFoundError(
-            f"{', '.join(missing)} missing, please run the dump commands above to get them."
-        )
-
-    # This checks if the INDRA DB Lite location is set. It is needed to
-    # significantly speed up the process of getting text refs in the
-    # assembly process
-    if not os.environ.get("INDRA_DB_LITE_LOCATION"):
-        raise ValueError("Environment variable 'INDRA_DB_LITE_LOCATION' not set")
-
-    # Check that the INDRA DB Lite location is valid
-    if not os.path.isfile(os.environ["INDRA_DB_LITE_LOCATION"]):
-        raise FileNotFoundError(
-            f"INDRA_DB_LITE_LOCATION={os.environ['INDRA_DB_LITE_LOCATION']} "
-            f"does not point to a valid file"
-        )
-    logger.info(
-        f"Found INDRA_DB_LITE_LOCATION={os.environ['INDRA_DB_LITE_LOCATION']}"
-    )
-
-    # This checks that a connection to the prinicipal db can be established.
-    # It is needed for the preassembly step as fallback when the
-    # indra_db_lite is missing content
-    from indra_db import get_db
-    db = get_db("primary")
-    if db is None:
-        raise ValueError("Could not connect to the principal db. Please set "
-                         "INDRADBPRIMARY to point to a valid db or add a db "
-                         "config to db_config.ini")
-    logger.info("Principal db is available")
-
-    # This checks if there are any adeft models available. They are needed to
-    adeft_model_count = len(get_available_models())
-    if adeft_model_count == 0:
-        raise ValueError(
-            "No adeft models detected, run 'python -m adeft.download' to "
-            "download models"
-        )
-    logger.info(f"Found {adeft_model_count} adeft models")
-
-    # STAGE 1: We need to run statement distillation to figure out which
-    # raw statements we should ignore based on the text content and
-    # reader version used to produce it.
-    if not drop_readings_fname.exists() or not reading_to_text_ref_map.exists():
-        logger.info("Running statement distillation")
-        logger.info("Loading reading and text content into dataframe")
-        df = pandas.read_csv(
-            reading_text_content_fname,
-            header=None,
-            sep="\t",
-            names=[
-                "reading_id",
-                "reader_version",
-                "text_content_id",
-                "text_ref_id",
-                "text_content_source",
-                "text_content_type",
-            ],
-        )
-        df.sort_values("text_ref_id", inplace=True)
-
-        drop_readings = set()
-        trid = df["text_ref_id"].iloc[0]
-        contents = defaultdict(list)
-
-        # This takes around ~10 min
-        for row in tqdm.tqdm(df.itertuples(),
-                             total=len(df),
-                             unit_scale=True,
-                             desc="Distilling statements"):
-            if row.text_ref_id != trid:
-                for reader, reader_contents in contents.items():
-                    if len(reader_contents) < 2:
-                        continue
-                    drop_new = reader_prioritize(reader_contents)
-                    # A sanity check to make sure we don't drop all
-                    # the readings
-                    assert len(drop_new) < len(reader_contents)
-                    drop_readings |= drop_new
-                contents = defaultdict(list)
-            contents[version_to_reader[row.reader_version]].append(
-                (
-                    row.reading_id,
-                    row.reader_version,
-                    row.text_content_source,
-                    row.text_content_type,
-                )
-            )
-            trid = row.text_ref_id
-
-        with open(drop_readings_fname, "wb") as fh:
-            logger.info(f"Dumping {drop_readings_fname.as_posix()}")
-            pickle.dump(drop_readings, fh)
-
-        # Dump mapping of reading_id to text_ref_id
-        reading_id_to_text_ref_id = dict(zip(df.reading_id, df.text_ref_id))
-        with reading_to_text_ref_map.open("wb") as fh:
-            logger.info(f"Dumping {reading_to_text_ref_map.as_posix()}")
-            pickle.dump(reading_id_to_text_ref_id, fh)
-
-    else:
-        logger.info(
-            f"Loading {drop_readings_fname.as_posix()} and {reading_to_text_ref_map.as_posix()} from cache"
-        )
-        with open(drop_readings_fname, "rb") as fh:
-            drop_readings = pickle.load(fh)
-        # Get mapping of reading_id to text_ref_id
-        with reading_to_text_ref_map.open("rb") as fh:
-            reading_id_to_text_ref_id = pickle.load(fh)
-
-    # STAGE 2: We now need to iterate over raw statements and do preassembly
-    # Takes ~16 h
-    if not processed_stmts_fname.exists() or not source_counts_fname.exists():
-        logger.info("Processing statements and collecting source counts")
-        text_refs = load_text_refs_by_trid(text_refs_fname.as_posix())
-        source_counts = defaultdict(lambda: defaultdict(int))
-
-        # Warm up bio_ontology
-        from indra.ontology.bio import bio_ontology
-        bio_ontology.initialize()
-
-        with gzip.open(raw_stmts_fname, "rt") as fh, gzip.open(
-            processed_stmts_fname, "wt"
-        ) as fh_out:
-            reader = csv.reader(fh, delimiter="\t")
-            writer = csv.writer(fh_out, delimiter="\t")
-            for lines in tqdm.tqdm(
-                batch_iter(reader, 10000),
-                total=7665,
-                unit_scale=True,
-                unit="batch",
-                desc="Processing statements"
-            ):
-                stmts_jsons = []
-                for raw_stmt_id, db_info_id, reading_id, stmt_json_raw in lines:
-                    # NOTE: We might want to propagate the raw_stmt_id for
-                    # use when constructing Evidence nodes in the ingestion
-                    # step.
-                    refs = None
-                    if reading_id != "\\N":
-                        # Skip if this is for a dropped reading
-                        if int(reading_id) in drop_readings:
-                            continue
-                        text_ref_id = reading_id_to_text_ref_id.get(int(reading_id))
-                        if text_ref_id:
-                            refs = text_refs.get(text_ref_id)
-                    stmt_json = load_stmt_json_str(stmt_json_raw)
-                    if refs:
-                        stmt_json["evidence"][0]["text_refs"] = refs
-                        if refs.get("PMID"):
-                            stmt_json["evidence"][0]["pmid"] = refs["PMID"]
-                    stmts_jsons.append(stmt_json)
-                stmts = stmts_from_json(stmts_jsons)
-                stmts = ac.fix_invalidities(stmts, in_place=True)
-                stmts = ac.map_grounding(stmts)
-                stmts = ac.map_sequence(stmts)
-                for stmt in stmts:
-                    stmt_hash = stmt.get_hash(refresh=True)
-                    source_counts[stmt_hash][stmt.evidence[0].source_api] += 1
-                rows = [(stmt.get_hash(), json.dumps(stmt.to_json())) for stmt in stmts]
-                writer.writerows(rows)
-
-        # Cast defaultdict to dict and pickle the source counts
-        logger.info("Dumping source counts")
-        source_counts = dict(source_counts)
-        with open(source_counts_fname.as_posix(), "wb") as fh:
-            pickle.dump(source_counts, fh)
-        # We can now remove reference to source counts
-        del source_counts
-
-    else:
-        logger.info(
-            f"Statements already preassembled at "
-            f"{processed_stmts_fname.as_posix()}, source counts "
-            f"already dumped to {source_counts_fname.as_posix()}, skipping..."
-        )
+    #Get the files from S3 of indra_db readonly pipeline
+    bucket = "indra-db-readonly"
+    s3_base_prefix = get_latest_timestamp_prefix(bucket, prefix="cogex_files/")
+    download_s3_file(bucket, f"{s3_base_prefix}processed_statements.tsv.gz",
+                     processed_stmts_fname)
+    download_s3_file(bucket, f"{s3_base_prefix}source_counts.pkl",
+                     source_counts_fname)
 
     # ONE STAGE: create grounded and unique dumps
     # from processed statement in readonly pipeline
