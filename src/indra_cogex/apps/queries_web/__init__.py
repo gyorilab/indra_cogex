3159f719843cf1202539888a96592dbf9e3c87b6
Prasham Marfatia <marfatia.p@northeastern.edu>
2025-04-18 10:19:20 -0400

    Expose the get_network_for_statements API endpoint

--- src/indra_cogex/apps/queries_web/__init__.py
+++ src/indra_cogex/apps/queries_web/__init__.py
@@ -202,7 +202,8 @@ FUNCTION_CATEGORIES = {
             "get_stmts_for_stmt_hashes",
             "get_statements",
             "get_mesh_annotated_evidence",
-            "get_network"
+            "get_network",
+            "get_network_for_statements"
         ]
     },
     'drug_targets': {
@@ -443,6 +444,7 @@ examples_dict = {
     "gene_names": fields.List(fields.String, example=continuous_analysis_example_names),
     "target_id": fields.String(example="hgnc:646"),
     "is_downstream": fields.Boolean(example=False),
+    "regulator_type": fields.String(example="kinase", enum=["kinase", "tf", None]),
     "minimum_evidence": fields.Float(example=2),
     "log_fold_change": fields.List(fields.Float, example=continuous_analysis_example_data),
     "species": fields.String(example="human"),
