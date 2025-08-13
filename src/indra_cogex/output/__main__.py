5ee61a76ff668e58e350acb5a33b0e6c56b0fc5a
kkaris <karis.klas@gmail.com>
2025-08-13 11:53:13 -0700

    Add script to dump new sif and hash-mesh map from CoGEx

---
+++ src/indra_cogex/output/__main__.py
@@ -0,0 +1,73 @@
+import pickle
+from pathlib import Path
+
+import click
+from datetime import datetime, UTC
+
+from depmap_analysis.util.aws import (
+    dump_pickle_to_s3,
+    NETS_PREFIX,
+    NETS_SOURCE_DATA_PREFIX_SUBDIR,
+    SIF_PKL_NAME,
+    STMT_HASH_MESH_PKL_NAME
+)
+
+from indra_cogex.client.neo4j_client import Neo4jClient
+from indra_cogex.output.network_search import sif_with_logp, get_stmt_hash_to_mesh_map
+
+
+@click.command()
+@click.option("--limit", type=int, default=None, help="Limit the number of relations to process")
+@click.option(
+    "--date",
+    type=str,
+    default=datetime.now(UTC).strftime("%Y-%m-%d"),
+    show_default=True,
+    help="Date for the dump prefix")
+@click.option(
+    "--skip-upload",
+    is_flag=True,
+    default=False,
+    help="Skip uploading the results to S3. Useful for local testing.",
+)
+def main(limit: int, date: str, skip_upload: bool):
+    """Main function to run the script.
+
+    Parameters
+    ----------
+    limit :
+        An optional limit on the number of relations to process.
+        If None, no limit is applied.
+    date :
+        The date to use for the S3 prefix in the format YYYY-MM-DD. Defaults to
+        today.
+    skip_upload :
+        If True, skip uploading the results to S3. Useful for local testing.
+    """
+    client = Neo4jClient()
+    sif_df = sif_with_logp(client, limit=limit)
+    mesh_map = get_stmt_hash_to_mesh_map(client, limit=limit)
+
+    # Save the results to the depmap-analysis bucket on S3
+    prefix = f"{NETS_PREFIX}/{date}/{NETS_SOURCE_DATA_PREFIX_SUBDIR}/".replace("//", "/")
+    if not skip_upload:
+        dump_pickle_to_s3(
+            pyobj=sif_df,
+            name=SIF_PKL_NAME,
+            prefix=prefix,
+        )
+        dump_pickle_to_s3(
+            pyobj=mesh_map,
+            name=STMT_HASH_MESH_PKL_NAME,
+            prefix=prefix,
+        )
+    else:
+        click.echo("Skipping upload to S3. Results are saved locally.")
+        sif_df.to_pickle(SIF_PKL_NAME)
+        Path(STMT_HASH_MESH_PKL_NAME).open("wb").write(
+            pickle.dumps(mesh_map)
+        )
+
+
+if __name__ == "__main__":
+    main()
