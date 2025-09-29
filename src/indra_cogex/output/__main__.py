676b9c5f94cbe72e091229379cffcd3bcdbd9235
kkaris <karis.klas@gmail.com>
2025-09-29 16:34:09 -0700

    Update CLI for network search SIF dumping

--- src/indra_cogex/output/__main__.py
+++ src/indra_cogex/output/__main__.py
@@ -1,6 +1,3 @@
-import pickle
-from pathlib import Path
-
 import click
 from datetime import datetime, UTC
 
@@ -9,15 +6,12 @@ from depmap_analysis.util.aws import (
     NETS_PREFIX,
     NETS_SOURCE_DATA_PREFIX_SUBDIR,
     SIF_PKL_NAME,
-    STMT_HASH_MESH_PKL_NAME
 )
 
-from indra_cogex.client.neo4j_client import Neo4jClient
-from indra_cogex.output.network_search import sif_with_logp, get_stmt_hash_to_mesh_map
+from indra_cogex.output.network_search import sif_with_logp
 
 
 @click.command()
-@click.option("--limit", type=int, default=None, help="Limit the number of relations to process")
 @click.option(
     "--date",
     type=str,
@@ -30,11 +24,9 @@ from indra_cogex.output.network_search import sif_with_logp, get_stmt_hash_to_me
     default=False,
     help="Skip uploading the results to S3. Useful for local testing.",
 )
-def main(limit: int, date: str, skip_upload: bool):
+def main(date: str, skip_upload: bool):
     """Main function dumping the latest SIF and statement hash-mesh ID mappings"""
-    client = Neo4jClient()
-    sif_df = sif_with_logp(client, limit=limit)
-    mesh_map = get_stmt_hash_to_mesh_map(client, limit=limit)
+    sif_df = sif_with_logp(method="ingestion_files")
 
     # Save the results to the depmap-analysis bucket on S3
     prefix = f"{NETS_PREFIX}/{date}/{NETS_SOURCE_DATA_PREFIX_SUBDIR}/".replace("//", "/")
@@ -44,18 +36,9 @@ def main(limit: int, date: str, skip_upload: bool):
             name=SIF_PKL_NAME,
             prefix=prefix,
         )
-        dump_pickle_to_s3(
-            pyobj=mesh_map,
-            name=STMT_HASH_MESH_PKL_NAME,
-            prefix=prefix,
-        )
     else:
-        click.echo("Skipping upload to S3. Results are saved locally.")
+        click.echo(f"Skipping upload to S3. Results are saved locally to {SIF_PKL_NAME}")
         sif_df.to_pickle(SIF_PKL_NAME)
-        Path(STMT_HASH_MESH_PKL_NAME).open("wb").write(
-            pickle.dumps(mesh_map)
-        )
-
 
 if __name__ == "__main__":
     main()
