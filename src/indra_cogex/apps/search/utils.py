5c989fc94db55d83a171c9436fd8ab4b2e22288c
kkaris <karis.klas@gmail.com>
2025-10-17 14:03:28 -0700

    Move assembled nodes path construction function to avoid sources module imports

--- src/indra_cogex/apps/search/utils.py
+++ src/indra_cogex/apps/search/utils.py
@@ -6,8 +6,7 @@ import pickle
 
 from indra_cogex.apps.constants import AGENT_NAME_CACHE
 from indra_cogex.client import Neo4jClient
-from indra_cogex.sources.cli import _get_assembled_path
-
+from indra_cogex.assembly import get_assembled_path
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +21,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if not AGENT_NAME_CACHE.exists() or args.force:
         # Load straight from the tsv.gz file if it exists
-        bioentity_path = _get_assembled_path("BioEntity")
+        bioentity_path = get_assembled_path("BioEntity")
         if bioentity_path.exists():
             logger.info("Re-generating BioEntity cache from tsv.gz file")
             agent_cache = set()
