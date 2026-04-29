8063ec128448f991b59eb8fee2f0eea6358d90e8
kkaris <karis.klas@gmail.com>
2026-04-29 15:10:00 -0700

    Extend __main__ file docstring to explain running submodule app

--- src/indra_cogex/apps/trial_results/__main__.py
+++ src/indra_cogex/apps/trial_results/__main__.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 
-"""Run the query web app with ``python -m indra_cogex.apps.trial_results.cli``."""
+"""Run the trial results web app with ``python -m indra_cogex.apps.trial_results.cli``.
+
+While this doesn't load the full CoGEx web app, it allows the trial results web
+application to be run as a standalone module, skipping the typical startup
+activities associated with running the full INDRA-CoGEx application.
+In this mode, most pages, including the INRA-CoGEx landing page, will not be
+available, but it's still possible to navigate directly to the trial results
+landing page docs at /trial-results/.
+"""
 
 from .cli import cli
 
